e52bdd90d10b3826732db12de3a1b33497ac7d8b
Ray Kuyvenhoven <rjkuyvenhoven@gmail.com>
2022-06-13 15:07:22 -0700

    Paragraph & Content Type Updates
    add descriptions to paragraphs; added Region Profile, Labour Market Monthly and Industry Profile content types; updated display to include SSoT fields for WorkBC Centre, Labour Market Monthly and Industry Profile content types

---
+++ src/web/modules/custom/workbc_extra_fields/src/Plugin/ExtraField/Display/RegionProfile/RegionUnemploymentRateRegion.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Drupal\workbc_extra_fields\Plugin\ExtraField\Display\RegionProfile;
+
+use Drupal\Core\Entity\ContentEntityInterface;
+use Drupal\Core\StringTranslation\StringTranslationTrait;
+use Drupal\extra_field\Plugin\ExtraFieldDisplayFormattedBase;
+use GuzzleHttp\Client;
+use GuzzleHttp\Exception\RequestException;
+
+/**
+ * Example Extra field with formatted output.
+ *
+ * @ExtraFieldDisplay(
+ *   id = "region_unemployment_rate_region",
+ *   label = @Translation("Region Unemployment Rate"),
+ *   description = @Translation("An extra field to display unemployment rate."),
+ *   bundles = {
+ *     "node.region_profile",
+ *   }
+ * )
+ */
+class RegionUnemploymentRate extends ExtraFieldDisplayFormattedBase {
+
+  use StringTranslationTrait;
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getLabel() {
+
+    return $this->t('Region Unemployment Rate');
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getLabelDisplay() {
+
+    return 'above';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function viewElements(ContentEntityInterface $entity) {
+
+    $output = "[not-yet-available]";
+
+    return [
+      ['#markup' => $output],
+    ];
+  }
+
+}
