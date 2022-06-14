6aea29057fc6c43b2e46fb908ff9d5b4152c6239
infojunkie <karim.ratib@gmail.com>
2022-06-13 21:49:04 -0700

    Content migration WIP

--- src/scripts/migration/migrate.php
+++ src/scripts/migration/migrate.php
@@ -1,111 +1,108 @@
 <?php
 
 /**
- * Usage: drush scr /path/to/migrate.php /path/to/migration.csv
+ * Usage: drush scr /path/to/migrate.php -- /path/to/migration.csv
  */
-
-$file = $extra[0];
+$file = array_key_exists(0, $extra) ? $extra[0] : __DIR__ . '/migration.csv';
 if (empty($file) or ($handle = fopen($file, "r")) === FALSE) {
-    die("[WorkBC Migration] Could not open $file\nUsage: drush scr migrate -- /path/to/ia.csv\n");
+    die("[WorkBC Migration] Could not open $file\nUsage: drush scr migrate -- /path/to/migration.csv\n");
 }
+print("Importing $file\n");
 
 // Find the menu where we will insert the content.
-// https://drupal.stackexchange.com/a/284276/767
+global $menu_link_storage, $menu_name, $menu_item_home;
 $menu_link_storage = \Drupal::entityTypeManager()->getStorage('menu_link_content');
 $menu_name = 'main';
-$menu = $menu_link_storage->loadByProperties(['menu_name' => $menu_name]);
 $menu_item_home = 'standard.front_page';
 
 // Types that we can import.
+// TODO Fill this.
 $types = [
-    'Landing page' => 'action',
     'Basic page' => 'page',
-    'Career profile' => 'career_profile',
 ];
 
 // Map of nodes that we created.
-// title => id
+// title => [id, parent title]
 // TODO Save this in SQLite.
+global $nodes;
 $nodes = [
 ];
 
+// FIRST PASS: Create all the nodes.
+print("Creating content...\n");
+$row = 0;
 while (($data = fgetcsv($handle)) !== FALSE) {
+    // Skip first 2 rows.
+    $row++;
+    if ($row < 3) continue;
+
     // Detect a type that we can import.
     $type = $data[9];
     if (!$type || !array_key_exists($type, $types)) {
         // TODO Log warning.
-        continue;
     }
 
     // Detect title and parent's title to create the hierarchy.
     $parent = NULL;
     $title = NULL;
-    for ($c = 5; $c >= 0; $c--) {
-        if ($data[$c]) {
+    for ($c = 4; $c >= 0; $c--) {
+        if (!empty($data[$c])) {
             $title = $data[$c];
             if ($c > 0) {
                 $parent = $data[$c-1];
                 if ($parent === $title) {
+                    $parent = $c > 1 ? $data[$c-2] : NULL;
+                }
+                if (empty($parent)) {
                     $parent = NULL;
                 }
             }
+            print("$title => $parent\n");
             break;
         }
     }
-    if (!$title) {
+    if (empty($title)) {
         // TODO Log warning.
+        print("Skipping empty page at row $row\n");
         continue;
     }
 
+    // TODO Detect the type from the sheet.
+    $type = 'page';
+
+    // TODO Add more fields from GatherContent.
     $fields = [
         'type' => $type,
         'title' => $title,
         'uid' => 1,
     ];
 
-    print("Creating new $type with title: \"$title\" and parent: \"$parent\"\n");
-    // Insert the node in the menu hierarchy.
-    //
-    // $node = Drupal::entityTypeManager()
-    //     ->getStorage('node')
-    //     ->create($fields);
-    // $node->save();
-    //
-    $nodes[$title] = NULL; // $node->id()
-    if ($parent && !array_key_exists($parent, $nodes)) {
-        // TODO Log warning.
-        print("  Parent node not found: \"$parent\"\n");
-    }
+    $node = Drupal::entityTypeManager()
+        ->getStorage('node')
+        ->create($fields);
+    $node->save();
+    $nodes[$title] = [
+        'id' => $node->id(),
+        'parent' => $parent,
+        'menu_id' => NULL,
+    ];
 }
 fclose($handle);
 
-/**
- * CODE SAMPLES
- */
-
-/**
- * Navigate the menu hierarchy.
- */
-// $top_level = NULL;
-// foreach ($main_menu as $menu_item) {
-//   $parent_id = $menu_item->getParentId();
-//   if (!empty($parent_id)) {
-//     $top_level = $parent_id;
-//     break;
-//   }
-// }
-
-/**
- * Create menu item.
- */
-// $menu_link_storage->create([
-//     'title' => 'My menu link title',
-//     'link' => ['uri' => 'internal:/my/path'],
-//     'menu_name' => $menu_name,
-//     'parent' => $menu_item_home,
-//     'expanded' => TRUE,
-//     'weight' => 0,
-// ])->save();
+// SECOND PASS: Create the menu hierarchy.
+print("Creating menu...\n");
+foreach ($nodes as $title => $node) {
+    $menu_link = $menu_link_storage->create([
+        'title' => $title,
+        'link' => ['uri' => "internal:/node/{$node['id']}"],
+        'menu_name' => $menu_name,
+        'parent' => $menu_item_home,
+        'expanded' => TRUE,
+        'weight' => 0,
+    ]);
+    $menu_link->save();
+    $nodes[$title]['menu_id'] = $menu_link->id();
+}
 
 /**
  * Access GatherContent item.
@@ -119,12 +116,12 @@ fclose($handle);
 //   ->setEmail($email)
 //   ->setApiKey($apiKey);
 // try {
-//     $item = $gc->itemGet(14507644);
+//     $item = $gc->itemGet(12593020);
 //     $template = $gc->templateGet($item->templateId);
+//     $component = $gc->componentGet('b9abb081-fb11-444a-96bb-04777aefc84a');
 // }
 // catch (\Exception $e) {
 //     echo 'ERROR: ' . $e->getMessage() . PHP_EOL;
-
 //     exit(1);
 // }
 // print_r($item->content);
