793bb40b7b067570021d35343d877786a67e21e3
jinchuantang <79226696+jinchuantang@users.noreply.github.com>
2022-06-23 15:46:17 -0700

    Update db.tf

--- terraform/dev/db.tf
+++ terraform/dev/db.tf
@@ -4,9 +4,9 @@ data "aws_rds_cluster" "postgres" {
   cluster_identifier      = "ceu-postgres-cluster"
 }
 
-#data "aws_secretsmanager_secret_version" "creds" {
-#  secret_id = "workbc-cc-db-creds"
-#}
+data "aws_secretsmanager_secret_version" "creds" {
+  secret_id = "workbc-cc-db-creds"
+}
 
 #locals {
 #  db_creds = jsondecode(
