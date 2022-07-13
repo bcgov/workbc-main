a6d2ec4d285be7b5b061c69d913a0fcc72edeeb9
jinchuantang <79226696+jinchuantang@users.noreply.github.com>
2022-07-13 10:12:49 -0700

    Update main.tf

--- terraform/dev/main.tf
+++ terraform/dev/main.tf
@@ -2,5 +2,5 @@
 locals {
   common_tags        = var.common_tags
   create_ecs_service = var.app_image == "" ? 0 : 1
-  conn_str           = "http://${data.aws_alb.ssot-lb.dns_name}:3000"
+  conn_str           = "http://${data.aws_alb.ssot.dns_name}:3000"
 }
