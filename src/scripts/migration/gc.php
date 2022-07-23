2a940f57c85a0ee87a7b9468b11120813ae9e5c5
infojunkie <karim.ratib@gmail.com>
2022-07-22 19:48:36 -0700

    Access GatherContent script for migration

---
+++ src/scripts/migration/gc.php
@@ -0,0 +1,43 @@
+<?php
+
+use EasyRdf\Parser\Json as ParserJson;
+use EasyRdf\Serialiser\Json;
+
+$email = $_ENV['GATHERCONTENT_EMAIL'];
+$apiKey = $_ENV['GATHERCONTENT_APIKEY'];
+
+$getopt = new \GetOpt\GetOpt([
+  ['h', 'help', \GetOpt\GetOpt::NO_ARGUMENT, 'Show this help and quit']
+], [\GetOpt\GetOpt::SETTING_STRICT_OPERANDS => true]);
+$getopt->addOperand(new \GetOpt\Operand('id', \GetOpt\Operand::REQUIRED));
+try {
+  $getopt->process($extra);
+}
+catch (Exception $e) {
+  die($getopt->getHelpText());
+}
+
+$client = new \GuzzleHttp\Client();
+$gc = new \Cheppers\GatherContent\GatherContentClient($client);
+$gc
+  ->setEmail($email)
+  ->setApiKey($apiKey);
+try {
+  $statuses = $gc->projectStatusesGet($getopt->getOperand('id'));
+  $publish_status = current(array_filter($statuses['data'], function ($status) {
+    return strcasecmp($status->name, 'publish') === 0;
+  }))->id;
+  $items = $gc->itemsGet($getopt->getOperand('id'), ['status_id' => [$publish_status]]);
+  $templates = [];
+  foreach ($items['data'] as $i) {
+    $item = $gc->itemGet($i->id);
+    if (!array_key_exists($item->templateId, $templates)) {
+      $templates[$item->templateId] = $gc->templateGet($item->templateId);
+    }
+    print($item->name . ' isA ' . $templates[$item->templateId]['data']->name . "\n");
+  }
+}
+catch (Exception $e) {
+    echo 'ERROR: ' . $e->getMessage() . PHP_EOL;
+    exit(1);
+}
