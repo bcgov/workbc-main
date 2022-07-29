bd0c2a581e3ac592839c854d399060ff0266aaf5
Dave <dave.kay@number41media.com>
2022-07-29 12:38:49 -0700

    modified the Carousel to always maintain a sibling on either side

--- src/web/themes/custom/workbc/js/related-topics-carousel.js
+++ src/web/themes/custom/workbc/js/related-topics-carousel.js
@@ -1,28 +1,48 @@
-// See https://codingyaar.com/bootstrap-carousel-multiple-items-increment-by-1/ for inspiration
-
 (function ($, Drupal) {
 
-    var carouselWidth = $(".carousel-inner")[0].scrollWidth;
-    
-    var cardWidth = $(".carousel-item").width();
-    
-    var scrollPosition = 0;
-    
-    $(".carousel-control-next").on("click", function () {
-      if (scrollPosition < (carouselWidth - cardWidth * 4)) { //check if you can go any further
-        scrollPosition += cardWidth;  //update scroll position
-        $(".carousel-inner").animate({ scrollLeft: scrollPosition },600); //scroll left
-      }
-    });
-    
-    $(".carousel-control-prev").on("click", function () {
-      if (scrollPosition > 0) {
-        scrollPosition -= cardWidth;
-        $(".carousel-inner").animate(
-          { scrollLeft: scrollPosition },
-          600
-        );
-      }
-    });
+  let carousel = $('#relatedTopicsCarousel');
+
+  let updateVisible = function () {
+
+    let items = carousel.find(".carousel-item");
+    let active = items.filter(".active");
+    let activeIndex = items.index(active);
+
+    // This should generally be handled by intercepting the bootstrap slide event, but we need this for initial load
+    if(activeIndex < 1) {
+      // don't allow 'active' to be at the very start, it always needs a previous sibling
+      active.removeClass('active');
+      $(items[1]).addClass('active');
+    } else if (activeIndex == items.length - 1) {
+      // don't allow 'active' to be at the very end - it always needs a next sibling
+      active.removeClass('active');
+      $(items[items.length - 2]).addClass('active');
+    }
+
+    // update index with changes
+    active = items.filter(".active");
+    activeIndex = items.index(active);
+
+    items.removeClass("prev");
+    items.removeClass("next");
+
+    $(items[activeIndex - 1]).addClass("prev");
+    $(items[activeIndex + 1]).addClass("next");
+  };
+
+  let checkBounds = function (event) {
+    // this prevents the Bootstrap code from allowing a slide to the very first or last item, since we always want to maintain siblings on either side
+    let items = carousel.find(".carousel-item");
+
+    if(event.type == 'slide' && (event.to == 0 || event.to == items.length - 1)) {
+        event.stopPropagation();
+        return false;
+    }
+  };
+
+  carousel.on('slide.bs.carousel', checkBounds);
+  carousel.on('slid.bs.carousel', updateVisible);
+
+  updateVisible();
 
 })(jQuery, Drupal);
