81513be85991855f81635ada91ac123180d4a3c4
Mirzero <Mirzero@users.noreply.github.com>
2022-08-09 08:50:16 -0700

    WR-490: Related Topics carousel no longer doubles up items when it has less than the max (#50)
    Co-authored-by: Dave <dave.kay@number41media.com>

--- src/web/themes/custom/workbc/js/related-topics-carousel.js
+++ src/web/themes/custom/workbc/js/related-topics-carousel.js
@@ -4,17 +4,19 @@
 
   let items = document.querySelectorAll('.carousel .carousel-item');
 
+  const minPerSlide = Math.min(items.length, 3);
+
   items.forEach((el) => {
-      const minPerSlide = 3;
-      let next = el.nextElementSibling
-      for (var i=1; i<minPerSlide; i++) {
-          if (!next) {
-              // wrap carousel by using first child
-            next = items[0]
-          }
-          let cloneChild = next.cloneNode(true)
-          el.appendChild(cloneChild.children[0])
-          next = next.nextElementSibling
+    let next = el.nextElementSibling;
+    for (var i = 1; i < minPerSlide; i++) {
+      if (!next) {
+          // wrap carousel by using first child
+        next = items[0]
       }
+      let cloneChild = next.cloneNode(true)
+      el.appendChild(cloneChild.children[0])
+      next = next.nextElementSibling
+    }
   })
+
 })(jQuery, Drupal);
