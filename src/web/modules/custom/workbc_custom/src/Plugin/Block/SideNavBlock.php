cf75a47b4928c4640c3f2e347788793f26dd4f79
Andrei Zvonkov <zvonkov@gmail.com>
2022-09-09 13:59:02 -0400

    WR-526: Page layout options (#82)
    * page config updates
    
    * node template override
    
    * narrow page layout
    
    * recompiled css
    
    * recompiled css
    
    * Add custom sidenavblock, this block will display if node has page format of "sidenav"
    
    Co-authored-by: rjkuyvenhoven <rjkuyvenhoven@gmail.com>

---
+++ src/web/modules/custom/workbc_custom/src/Plugin/Block/SideNavBlock.php
@@ -0,0 +1,119 @@
+<?php
+
+namespace Drupal\workbc_custom\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Drupal\Core\Routing\RouteMatchInterface;
+use Drupal\node\NodeInterface;
+use Drupal\Core\Cache\Cache;
+use Drupal\Core\Access\AccessResult;
+use Drupal\Core\Session\AccountInterface;
+
+/**
+ * Provides a 'Side Navigation' block.
+ *
+ * @Block(
+ *  id = "side_nav_block",
+ *  label = "Side Nav for Page content type",
+ *  admin_label = @Translation("Side Nav Block"),
+ * )
+ */
+class SideNavBlock extends BlockBase implements ContainerFactoryPluginInterface {
+
+  /**
+   * The current route match.
+   *
+   * @var \Drupal\Core\Routing\RouteMatchInterface
+   */
+  protected $routeMatch;
+
+  /**
+   * Constructs a new BookNavigationBlock instance.
+   *
+   * @param array $configuration
+   *   A configuration array containing information about the plugin instance.
+   * @param string $plugin_id
+   *   The plugin_id for the plugin instance.
+   * @param mixed $plugin_definition
+   *   The plugin implementation definition.
+   * @param \Drupal\Core\Routing\RouteMatchInterface $route_match
+   *   The current route match.
+   */
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, RouteMatchInterface $route_match) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition);
+
+    $this->routeMatch = $route_match;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->get('current_route_match')
+    );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build() {
+
+    $markup = '<div class="basic-page-left-nav-wrapper">';
+    $markup .= '<nav class="basic-page-left-nav">';
+    $markup .= '<h2>On this page</h2>';
+    $markup .= '<ul>';
+    $markup .= '<li><a href="#heading1">Link to heading 1</a></li>';
+    $markup .= '<li><a href="#heading2">Link to heading 2</a></li>';
+    $markup .= '<li><a href="#heading3">Link to heading 3</a></li>';
+    $markup .= "</ul>";
+    $markup .= "</nav>";
+    $markup .= "</div>";
+    return [
+      '#markup' => $markup,
+    ];
+  }
+
+
+  /**
+   * Only display block if the node has a page format field and it has a
+   * value of "sidenav";
+   */
+  protected function blockAccess(AccountInterface $account) {
+    $node = $this->routeMatch->getParameter('node');
+    if (empty($node)) {
+      return AccessResult::forbidden();
+    }
+    if (!$node->hasField("field_page_format")) {
+      return AccessResult::forbidden();
+    }
+    if ($node->field_page_format->value <> "sidenav") {
+      return AccessResult::forbidden();
+    }
+    return AccessResult::allowedIfHasPermission($account, 'access content');
+  }
+
+
+  public function getCacheTags() {
+    // With this when your node change your block will rebuild.
+    if ($node = \Drupal::routeMatch()->getParameter('node')) {
+      // If there is node add its cachetag.
+      return Cache::mergeTags(parent::getCacheTags(), ['node:' . $node->id()]);
+    }
+    else {
+      // Return default tags instead.
+      return parent::getCacheTags();
+    }
+  }
+
+  public function getCacheContexts() {
+    // Every new route this block will rebuild.
+    return Cache::mergeContexts(parent::getCacheContexts(), ['route']);
+  }
+
+}
