7e69140a395177782109312813d076a0dd544373
Dave <dave.kay@number41media.com>
2022-09-12 09:29:55 -0700

    Added JS to the module/block to set up all of the leftnav anchor links from the body content

---
+++ src/web/themes/custom/workbc/js/sidenav-anchors.js
@@ -0,0 +1,35 @@
+(function ($, Drupal, once) {
+  ("use strict");
+
+  let linkList = jQuery('ul.basic-page-left-nav-links');
+
+  let configureHeadingAnchor = function (index, element) {
+    var self = this;
+
+    var anchorid = "sideNavAnchorId_" + index;
+    var anchorText = jQuery(element).text();
+
+    $(this).wrapInner('<a id="' + anchorid + '" />');
+
+    linkList.append('<li><a href="#' + anchorid + '">' + anchorText + '</a></li>');
+  };
+
+  let initSidenavAnchors = function() {
+    console.log('Begin sidenav configuration');
+
+    let article = jQuery('article.page-format--sidenav');
+    let headings = article.find('h2');
+
+    headings.each(configureHeadingAnchor);
+
+    console.log('End sidenav configuration');
+  };
+
+  Drupal.behaviors.sidenavAnchors = {
+    attach: function (context, settings) {
+      // the second parameter must be a selector specific to the content this script applies to, to ensure it's loaded after the content in the case the content is lazy loaded by Drupal
+      once('sidenavAnchors', '.basic-page-left-nav-wrapper', context).forEach(initSidenavAnchors);
+    },
+  };
+
+})(jQuery, Drupal, once);
