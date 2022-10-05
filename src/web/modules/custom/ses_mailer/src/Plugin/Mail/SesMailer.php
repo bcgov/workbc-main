f37d420e6f48ed15b977dabd6751ca973d5ddedd
rjkuyvenhoven <rjkuyvenhoven@gmail.com>
2022-10-05 09:54:48 -0700

    WR-552-content-edit-work-flow
    commit 63205e2851d974f0cafe23b3e22cc9a8d694c480
    Merge: 9c2bb1ee 010213ec
    Author: rjkuyvenhoven <rjkuyvenhoven@gmail.com>
    Date:   Wed Oct 5 09:52:58 2022 -0700
    
        Merge branch 'master' into WR-552-content-edit-work-flow
    
    commit 9c2bb1ee9f86b5b2948e87615e6b0ae860c6e90f
    Author: rjkuyvenhoven <rjkuyvenhoven@gmail.com>
    Date:   Wed Oct 5 09:42:41 2022 -0700
    
        convert TO, CC and BCC values to arrays if necessary

--- src/web/modules/custom/ses_mailer/src/Plugin/Mail/SesMailer.php
+++ src/web/modules/custom/ses_mailer/src/Plugin/Mail/SesMailer.php
@@ -89,16 +89,41 @@ class SesMailer extends PluginBase implements MailInterface, ContainerFactoryPlu
   public function mail(array $message) {
     $result = [];
     $result['error'] = FALSE;
+
     try {
       // Credentials are set in environment variables.
-      $cc = array_key_exists('Cc', $message['headers']) ? [$message['headers']['Cc']] : [];
-      $bcc = array_key_exists('Bcc', $message['headers']) ? [$message['headers']['Bcc']] : [];
 
+      if (!is_array($message['to'])) {
+        if (!empty($message['to'])) {
+          $to = explode(",", $message['to']);
+        }
+        else {
+          $to = [];
+        }
+      }
+      if (!is_array($message['headers']['Cc'])) {
+        if (!empty($message['headers']['Cc'])) {
+          $cc = explode(",", $message['headers']['Cc']);
+        }
+        else {
+          $cc = [];
+        }
+      }
+      if (!is_array($message['headers']['Bcc'])) {
+        if (!empty($message['headers']['Bcc'])) {
+          $bcc = explode(",", $message['headers']['Bcc']);
+        }
+        else {
+          $bcc = [];
+        }
+      }
+
+      // if no reply-to address is provided, default to using from address
       $replyTo = empty($message['reply-to']) ? $message['from'] : $message['reply-to'];
 
       $response = $this->sesClient->sendEmail([
         'Destination' => [
-          'ToAddresses' => [$message['to']],
+          'ToAddresses' => $to,
           'CcAddresses' => $cc,
           'BccAddresses' => $bcc,
         ],
