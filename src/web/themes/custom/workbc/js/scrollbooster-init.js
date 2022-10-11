e287d98c748d17111accc597ca83397c514c1c75
Dave <dave.kay@number41media.com>
2022-10-11 09:57:03 -0700

    Fixed an issue with scrollbooster in mobile

--- src/web/themes/custom/workbc/js/scrollbooster-init.js
+++ src/web/themes/custom/workbc/js/scrollbooster-init.js
@@ -9,15 +9,12 @@
           direction: "horizontal",
           scrollMode: "transform"
         });
-    
-        console.log("init scroll");
     };
     
     Drupal.behaviors.scroll_booster = {
       attach: function (context, settings) {
         // the second parameter must be a selector specific to the content this script applies to, to ensure it's loaded after the content in the case the content is lazy loaded by Drupal
         once('scroll_booster', '.scroll-container', context).forEach(initScrollbooster);
-        console.log("once");
       },
     };
   
