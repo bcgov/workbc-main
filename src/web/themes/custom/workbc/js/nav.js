1a73d8a94f2ec5de1e198887f3bb202d84b0dd0a
Andrei Zvonkov <zvonkov@gmail.com>
2022-10-17 22:55:55 -0400

    WR-349: Text resizing (#127)
    * installed and enabled text resize module
    
    * text resize configs
    
    * removed placeholder blocks
    
    * text resize template override
    
    * footer template updates
    
    * text resize styles
    
    * footer layout updates
    
    * sticky nav cleanup
    
    * recompiled css

--- src/web/themes/custom/workbc/js/nav.js
+++ src/web/themes/custom/workbc/js/nav.js
@@ -4,19 +4,12 @@
   // Scroll detection for Banner Top offset.
   Drupal.behaviors.bannerTop = {
     attach: function (context, settings) {
-      // add class to body if top banner is present
-      $(document).ready(function () {
-        if ($(".alert-ribbon").length) {
-          $("body").addClass("page-has-alert");
-        }
-      });
-
       // detect scroll and add class to body
       $(window).scroll(function () {
         var y_scroll_pos = window.pageYOffset;
-        var scroll_pos_test = 142;
+        var scroll_pos = 142;
 
-        if (y_scroll_pos > scroll_pos_test) {
+        if (y_scroll_pos > scroll_pos) {
           $("body").addClass("nav-fixed");
         } else {
           $("body").removeClass("nav-fixed");
