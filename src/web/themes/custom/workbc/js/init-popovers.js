1cf4f59a1203ca1be1cdfd374d82c3d81c0cf056
Mirzero <Mirzero@users.noreply.github.com>
2022-10-21 15:21:46 -0700

    WR-361 - Implemented theme for Career Overview tab content (#138)
    * Fixed issues with the breadcrumb/hero/social-icons ordering between Desktop and Mobile views.
    
    * Implemented more of the tab content template/styling
    
    * Fixed a style issue with the Career Profile stats cards
    
    * Removed from styling from the old approach that is no longer needed and was causing some issues
    
    * Heavily reorganized and refactored the Career Profile template into something more manageable
    
    * Added Work Environment Stats
    
    * Added Career Pathways section
    
    * Added Related Careers section
    
    * Fixed some styling to account for actual data structure
    
    * Configuration: removed some titles from display
    
    * Career Profiles > Career Overview - Implemented the Occupational Interests section
    
    * Fixed an issue w/ popover code, and removed some spurious image files
    
    * All Career Profile popovers set to work on click and hover
    
    * Career Overview > Added Job Titles section
    
    Co-authored-by: Dave <dave.kay@number41media.com>

--- src/web/themes/custom/workbc/js/init-popovers.js
+++ src/web/themes/custom/workbc/js/init-popovers.js
@@ -1,9 +1,33 @@
 (function ($, Drupal, once) {
   ("use strict");
 
+  // this set of functions is intended to cause popovers to close if the user clicks anywhere outside of them
+  // see https://stackoverflow.com/a/69602400/495000
+  let managePopoverClosure = function () {
+    $(document).on('click', function (e) {
+      var $popover,
+          $target = $(e.target);
+      //do nothing if there was a click on popover content
+      if ($target.hasClass('popover') || $target.closest('.popover').length) {
+          return;
+      }
+      $('[data-bs-toggle="popover"]').each(function () {
+          $popover = $(this);
+  
+          if (!$popover.is(e.target) &&
+              $popover.has(e.target).length === 0 &&
+              $('.popover').has(e.target).length === 0)
+          {
+              $popover.popover('hide');
+          } 
+      });
+    })
+  }
+
   let initPopovers = function () {
     if($().popover) {
       $('[data-bs-toggle="popover"]').popover();
+      managePopoverClosure();
     }
   };
 
