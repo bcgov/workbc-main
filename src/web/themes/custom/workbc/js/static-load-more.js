0bb1624ef194d6b1fa40ff18902a43d1fd0d5bfd
Dave <dave.kay@number41media.com>
2022-10-26 09:17:31 -0700

    Career Profiles > Education, Skills and Training - Implemented the 'Load More' functionality for Skills

---
+++ src/web/themes/custom/workbc/js/static-load-more.js
@@ -0,0 +1,36 @@
+(function ($, Drupal, once) {
+  ("use strict");
+
+  let initialize = function (containerJquery) {
+    let container = $(containerJquery);
+    let initialCount = container.data('static-load-more-initial');
+    let items = container.children('[data-static-load-more-items]').first().children();
+    let trigger = container.children('[data-static-load-more-trigger]').first();
+
+    items.slice(initialCount).hide();
+
+    trigger.on('click', function() { loadMore(container) });
+
+    container.show();
+  };
+
+  let loadMore = function (containerJquery) {
+    let container = $(containerJquery);
+    let stepCount = container.data('static-load-more-step');
+    let hiddenItems = container.children('[data-static-load-more-items]').first().children(':hidden');
+    
+    hiddenItems.slice(0, stepCount).show();
+      
+    if(hiddenItems.length <= stepCount) {
+      let trigger = container.children('[data-static-load-more-trigger]').first();
+      trigger.hide();
+    }
+  }
+
+  Drupal.behaviors.initStaticLoadMoreBehavior = {
+    attach: function (context, settings) {
+      once('initStaticLoadMoreBehavior', '[data-static-load-more-container]', context).forEach(initialize);
+    },
+  };
+
+})(jQuery, Drupal, once);
