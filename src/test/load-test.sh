5d0ad162fea6ada488a6db159020647036c4ba1f
Karim Ratib <karim.ratib@gmail.com>
2022-12-08 19:01:51 -0800

    [WR-1216] Load testing (#241)

---
+++ src/test/load-test.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+xmllint --xpath "//*[local-name()='loc']/text()" $BASE_URL/sitemap.xml > urls.txt
+echo "Extracted $(wc -l urls.txt | awk '{print $1}') URLs from $BASE_URL/sitemap.xml. Running siege..."
+siege --rc=./siege.conf
