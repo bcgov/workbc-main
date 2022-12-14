57747aca56ce25e1c70f7dbd06d37cd5a95a1a2a
guopeiyaopearl <87669294+guopeiyaopearl@users.noreply.github.com>
2022-12-14 14:34:12 -0800

    Update cloudfront.tf

--- terraform/dev/cloudfront.tf
+++ terraform/dev/cloudfront.tf
@@ -75,12 +75,11 @@ resource "aws_cloudfront_distribution" "workbc" {
 
   tags = var.common_tags
   
-  #aliases = ["www.aws.workbc.ca"]
+  aliases = ["aws.workbc.ca"]
   #aliases = ["aws-dev.workbc.ca"]	
 
   viewer_certificate {
-    #acm_certificate_arn = "arn:aws:acm:us-east-1:873424993519:certificate/0215bb2d-d224-4681-bf6b-227e9e82f29f"	  
-    acm_certificate_arn = "arn:aws:acm:us-east-1:873424993519:certificate/63c7d3df-3db9-4799-84d4-897109ce6461"
+    acm_certificate_arn = "arn:aws:acm:us-east-1:873424993519:certificate/0215bb2d-d224-4681-bf6b-227e9e82f29f"
     ssl_support_method = "sni-only"
   }
 }
