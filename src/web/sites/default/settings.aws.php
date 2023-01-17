a76818d61f366762b36668fb94187853c8f95b61
infojunkie <karim.ratib@gmail.com>
2023-01-17 15:17:57 -0800

    [WR-1490] Fix sitemap base_url

--- src/web/sites/default/settings.aws.php
+++ src/web/sites/default/settings.aws.php
@@ -7,6 +7,7 @@ $base_urls = [
 ];
 if (array_key_exists(getenv('PROJECT_ENVIRONMENT'), $base_urls)) {
     $base_url = $base_urls[getenv('PROJECT_ENVIRONMENT')];
+    $config['simple_sitemap.settings']['base_url'] = $base_url;
 }
 
 $databases['default']['default'] = array (
