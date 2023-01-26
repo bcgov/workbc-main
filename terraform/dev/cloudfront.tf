68b5dc103e90c91aab0f73be07e2f6e61fb1fbde
jinchuantang <79226696+jinchuantang@users.noreply.github.com>
2023-01-26 15:10:50 -0800

    Update cloudfront.tf

--- terraform/dev/cloudfront.tf
+++ terraform/dev/cloudfront.tf
@@ -39,7 +39,7 @@ resource "aws_cloudfront_distribution" "workbc" {
   }
 	
   origin {
-    domain_name = aws_s3_bucket.workbc_s32.bucket_regional_domain_name
+        domain_name = aws_s3_bucket.workbc_s32.bucket_regional_domain_name
 	origin_id = "SDPR-Contents"
 	origin_access_control_id = aws_cloudfront_origin_access_control.oac.id
   }
