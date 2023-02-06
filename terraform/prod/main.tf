bfe25ca158e51530c1ef0f401f26b5482d922225
guopeiyaopearl <87669294+guopeiyaopearl@users.noreply.github.com>
2023-02-06 11:55:28 -0800

    WR-1220 Prepare for going live

--- terraform/prod/main.tf
+++ terraform/prod/main.tf
@@ -3,6 +3,7 @@ locals {
   common_tags        = var.common_tags
   create_ecs_service = var.app_image == "" ? 0 : 1
   conn_str           = "http://${data.aws_alb.ssot.dns_name}:3000"
-  jb_api_url         = "https://aws-api-jobboard.workbc.ca"
+  #jb_api_url         = "https://aws-api-jobboard.workbc.ca"
+  jb_api_url         = "https://api-jobboard.workbc.ca"
   jb_api_internal_url = "https://workbc-jb.b89n0c-prod.nimbus.cloud.gov.bc.ca"
 }
