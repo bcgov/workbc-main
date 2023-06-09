07d22c395ec3180001852d194860db77b433e0c5
Karim Ratib <karim.ratib@gmail.com>
2023-06-08 19:34:05 -0700

    [WR-1387] Fix profile saved status (#357)
    * Fix Job Seeker login detection
    * Reflect profile status

--- src/web/modules/custom/workbc_jobboard/js/workbc_jobboard.js
+++ src/web/modules/custom/workbc_jobboard/js/workbc_jobboard.js
@@ -7,7 +7,7 @@
         }).once('jobboard').click(function(e) {
           var url = $(this).attr('href');
           let domain = (new URL(url));
-          domain = domain.hostname.replace('www.',''); 
+          domain = domain.hostname.replace('www.','');
           domain = domain.split(".");
           domain.pop();
           domain = domain.join(' ');
@@ -28,30 +28,53 @@
         var currentUser = readCookie('currentUser.username');
         var CheckLoginLinkExists = $("nav.nav-user .nav-items li.new-login-link");
         var CheckLogoutLinkExists = $("nav.nav-user .nav-items li.new-logout-link");
-        
+
         var CheckLoginLinkMobileExists = $(".mobile-nav__user-nav .nav-items li.new-login-link");
         var CheckLogoutLinkMobileExists = $(".mobile-nav__user-nav .nav-items li.new-logout-link");
-        
+
           if(currentUser != ''){
             //
             if(CheckLoginLinkExists.length < 1){
               CheckLogoutLinkExists.remove();
               CheckLogoutLinkMobileExists.remove();
-              var appendLoginMenus = "<li class='nav-item new-login-link'> <a  href='/account#/dashboard' class='nav-link'>My Profile</a><li class='nav-item new-login-link'> <a  href='/account#/personal-settings' class='nav-link'>Personal Settings</a><li class='nav-item new-login-link'> <a  href='/account#/logout' class='nav-link' onclick=\"document.cookie = 'currentUser.username=; expires=Thu, 01 Jan 1970 00:00:00 UTC; Path=/'; document.cookie = 'currentUser.email=; expires=Thu, 01 Jan 1970 00:00:00 UTC; Path=/';document.cookie = 'currentUser.firstname=; expires=Thu, 01 Jan 1970 00:00:00 UTC; Path=/';document.cookie = 'currentUser.lastname=; expires=Thu, 01 Jan 1970 00:00:00 UTC; Path=/';document.cookie = 'currentUser.id=; expires=Thu, 01 Jan 1970 00:00:00 UTC; Path=/';document.cookie = 'currentUser.token=; expires=Thu, 01 Jan 1970 00:00:00 UTC; Path=/';localStorage.removeItem('currentUser'); location.reload();\"> Log out </a>";
-              
-              var appendLoginMenusD = "<li class='nav-item new-login-link dropdown'> <a  href='javascript:void(0)' class='nav-link dropdown-toggle' data-bs-toggle='dropdown'>My Account</a><ul class='dropdown-menu'><li class='nav-item new-login-link dropdown-item'><a  href='/account#/dashboard' class='nav-link' >My Profile</a></li><li class='nav-item new-login-link dropdown-item'><a  href='/account#/personal-settings' class='nav-link' >Personal Settings</a></li><li class='nav-item new-login-link dropdown-item'> <a  href='/account#/logout' class='nav-link' onclick=\"document.cookie = 'currentUser.username=; expires=Thu, 01 Jan 1970 00:00:00 UTC; Path=/'; document.cookie = 'currentUser.email=; expires=Thu, 01 Jan 1970 00:00:00 UTC; Path=/';document.cookie = 'currentUser.firstname=; expires=Thu, 01 Jan 1970 00:00:00 UTC; Path=/';document.cookie = 'currentUser.lastname=; expires=Thu, 01 Jan 1970 00:00:00 UTC; Path=/';document.cookie = 'currentUser.id=; expires=Thu, 01 Jan 1970 00:00:00 UTC; Path=/';document.cookie = 'currentUser.token=; expires=Thu, 01 Jan 1970 00:00:00 UTC; Path=/';localStorage.removeItem('currentUser'); location.reload();\"> Log out </a></li></ul></li>";
-              
+              var appendLoginMenusM = `
+<li class="nav-item new-login-link">
+  <a href="/account#/dashboard" class="nav-link">My Profile</a>
+</li>
+<li class="nav-item new-login-link">
+  <a  href="/account#/personal-settings" class="nav-link">Personal Settings</a>
+</li>
+<li class="nav-item new-login-link">
+  <a href="/account#/logout" class="nav-link" onclick="localStorage.removeItem('currentUser'); document.cookie='currentUser.username=; expires=Thu, 01 Jan 1970 00:00:00 UTC; Path=/;'; document.cookie='currentUser.email=; expires=Thu, 01 Jan 1970 00:00:00 UTC; Path=/;'; document.cookie='currentUser.firstName=; expires=Thu, 01 Jan 1970 00:00:00 UTC; Path=/;'; document.cookie='currentUser.lastName=; expires=Thu, 01 Jan 1970 00:00:00 UTC; Path=/;'; document.cookie='currentUser.id=; expires=Thu, 01 Jan 1970 00:00:00 UTC; Path=/;'; document.cookie='currentUser.token=; expires=Thu, 01 Jan 1970 00:00:00 UTC; Path=/;'; return true;">Log out</a>
+</li>
+`;
+              var appendLoginMenusD = `
+<li class="nav-item new-login-link dropdown">
+  <a  href="javascript:void(0)" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">My Account</a>
+  <ul class="dropdown-menu">
+    ${appendLoginMenusM}
+  </ul>
+</li>
+`;
+
               //Desktop menu
               $("nav.nav-user .nav-items").append(appendLoginMenusD);
-              
+
               //Mobile Menu
-              $(".mobile-nav__user-nav .nav-items").append(appendLoginMenus);
+              $(".mobile-nav__user-nav .nav-items").append(appendLoginMenusM);
             }
           }else{
             if(CheckLogoutLinkExists.length < 1){
               CheckLoginLinkExists.remove();
               CheckLoginLinkMobileExists.remove();
-              var appendLogoutMenus = "<li class='nav-item new-logout-link'> <a href='/account#/login' class='nav-link' > Log in </a><li class='nav-item new-logout-link'> <a href='/account#/register' class='nav-link' > Register </a>";
+              var appendLogoutMenus = `
+<li class="nav-item new-logout-link">
+  <a href="/account#/login" class="nav-link">Log in</a>
+</li>
+<li class="nav-item new-logout-link">
+  <a href="/account#/register" class="nav-link">Register</a>
+</li>
+`;
               $("nav.nav-user .nav-items").append(appendLogoutMenus);
               $(".mobile-nav__user-nav .nav-items").append(appendLogoutMenus);
             }
@@ -66,7 +89,7 @@
           $(this).parent().addClass('open');
          }
       });
-    } 
+    }
   }
 })(jQuery, Drupal);
 
@@ -77,6 +100,23 @@ if (window.location.hash) {
 
 
 function readCookie(cookieName){
+  if (window.localStorage) {
+    const split = cookieName.split('.');
+    try {
+      const entry = JSON.parse(window.localStorage.getItem(split[0]));
+
+      // Set each cookie.
+      for (const prop in entry) {
+        document.cookie= `${split[0]}.${prop}=${entry[prop]}; Path=/;`;
+      }
+
+      return entry[split[1]];
+    }
+    catch {
+      // Do nothing, continue with the cookie access.
+    }
+  }
+
   var d=[],
   e=document.cookie.split(";");
   cookieName=RegExp("^\\s*"+cookieName+"=\\s*(.*?)\\s*$");
@@ -84,5 +124,5 @@ function readCookie(cookieName){
     var f=e[b].match(cookieName);
     f&&d.push(f[1])
   }
-  return d;
+  return d[0] || '';
 }
--- src/web/modules/custom/workbc_jobboard/src/Form/JobboardSaveProfileForm.php
+++ src/web/modules/custom/workbc_jobboard/src/Form/JobboardSaveProfileForm.php
@@ -5,15 +5,11 @@ use Drupal\Core\Form\FormBase;
 use Drupal\Core\Form\FormStateInterface;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Drupal\workbc_jobboard\Controller\WorkBcJobboardController;
-use Drupal\Core\Ajax\AjaxResponse;
-use Drupal\Core\Ajax\HtmlCommand;
-use Drupal\Core\Ajax\ReplaceCommand;
-use Symfony\Component\HttpFoundation\Cookie;
 
 class JobboardSaveProfileForm extends FormBase {
-  
+
   public $isSaved = false;
-  
+
   /**
    * {@inheritdoc}
    */
@@ -44,18 +40,20 @@ class JobboardSaveProfileForm extends FormBase {
       }
     }
   }
-  
+
   /**
    * {@inheritdoc}
    */
   public function getFormId() {
-    return 'workbc_fobboard_save_profile';
+    return 'workbc_jobboard_save_profile';
   }
-  
+
   /**
    * {@inheritdoc}
    */
   public function buildForm(array $form, FormStateInterface $form_state) {
+    if(!isset($_COOKIE['currentUser_token'])) return $form;
+
     $form['search__form'] = [
       '#type' => 'markup',
       '#prefix' => '<div id="save_career_profile" class="save_career_profile">',
@@ -63,59 +61,43 @@ class JobboardSaveProfileForm extends FormBase {
     ];
     $form['search__form']['submit'] = [
       '#type' => 'submit',
-      '#value' => (!empty($this->isSaved))? t('Saved'): t('Save this profile'),
+      '#value' => !empty($this->isSaved) ? t('Saved') : t('Save this profile'),
+      '#disabled' => !empty($this->isSaved)
     ];
-    if(!empty($this->isSaved)){
-      $form['search__form']['submit']['#attributes'] = [
-        "onclick" => "javascript: this.submit; this.disabled = true;"
-      ];
-    }
     return $form;
   }
-  
-  /**
-   * {@inheritdoc}
-   */
-  public function validateForme($element, FormStateInterface $form_state) {
-    if(!empty($this->isSaved)){
-      $form_state->setError($element, t('Profile already saved.'));
-    }else{
-      parent::validateForm($form, $form_state);
-    }
-  }
-  
+
   /*
    * {@inheritdoc}
    */
   public function submitForm(array &$form, FormStateInterface $form_state) {
-    $response = [];
-    if(empty($this->isSaved)){
-        $Bearer = "Bearer ".$_COOKIE['currentUser_token'];
-        $node = \Drupal::routeMatch()->getParameter('node');
-        if($node instanceof \Drupal\node\NodeInterface) {
-          $type = $node->bundle();
-          if(isset($_COOKIE['currentUser_token'])){
-            if($type == 'career_profile') {
-              $temp_field_value = ($node->get('field_noc')->getValue())? $node->get('field_noc')->getValue(): '';
-              $temp_field_value = (!empty($temp_field_value) && isset($temp_field_value[0]['value']))? $temp_field_value[0]['value'] :'';
-              $action = 'saveProfile';
-            }
-            else if($type == 'industry_profile') {
-              $temp_field_value = ($node->get('field_job_board_save_profile_id')->getValue())? $node->get('field_job_board_save_profile_id')->getValue(): '';
-              $temp_field_value = (!empty($temp_field_value) && isset($temp_field_value[0]['value']))? $temp_field_value[0]['value'] :'';
-              $temp_field_value = explode(',', $temp_field_value);
-              $temp_field_value = $temp_field_value[0];
-              $action = 'saveIndustryProfile';
-            }
-            
-            $WorkBcJobboardController = new WorkBcJobboardController();
-            $parameters = ['profile_id'=>$temp_field_value, 'Authorization'=>$Bearer];
-            $saveProfile = $WorkBcJobboardController->getPosts($parameters, $action);
-            if($saveProfile['response'] == 200){
-              $this->isSaved = 1;
-              $message = 'Profile successfully added.';
-              \Drupal::messenger()->addMessage($message);
-            }
+    if(isset($_COOKIE['currentUser_token']) && empty($this->isSaved)){
+      $Bearer = "Bearer ".$_COOKIE['currentUser_token'];
+      $node = \Drupal::routeMatch()->getParameter('node');
+      if($node instanceof \Drupal\node\NodeInterface) {
+        $type = $node->bundle();
+        if(isset($_COOKIE['currentUser_token'])){
+          if($type == 'career_profile') {
+            $temp_field_value = ($node->get('field_noc')->getValue())? $node->get('field_noc')->getValue(): '';
+            $temp_field_value = (!empty($temp_field_value) && isset($temp_field_value[0]['value']))? $temp_field_value[0]['value'] :'';
+            $action = 'saveProfile';
+          }
+          else if($type == 'industry_profile') {
+            $temp_field_value = ($node->get('field_job_board_save_profile_id')->getValue())? $node->get('field_job_board_save_profile_id')->getValue(): '';
+            $temp_field_value = (!empty($temp_field_value) && isset($temp_field_value[0]['value']))? $temp_field_value[0]['value'] :'';
+            $temp_field_value = explode(',', $temp_field_value);
+            $temp_field_value = $temp_field_value[0];
+            $action = 'saveIndustryProfile';
+          }
+
+          $WorkBcJobboardController = new WorkBcJobboardController();
+          $parameters = ['profile_id'=>$temp_field_value, 'Authorization'=>$Bearer];
+          $saveProfile = $WorkBcJobboardController->getPosts($parameters, $action);
+          if($saveProfile['response'] == 200){
+            $this->isSaved = 1;
+            $message = 'Profile successfully added.';
+            \Drupal::messenger()->addMessage($message);
+          }
         }
         else {
           $session = \Drupal::request()->getSession();
@@ -126,9 +108,5 @@ class JobboardSaveProfileForm extends FormBase {
         }
       }
     }
-    else{
-      $message = 'Profile already saved.';
-      \Drupal::messenger()->addMessage($message);
-    }
-  } 
+  }
 }
