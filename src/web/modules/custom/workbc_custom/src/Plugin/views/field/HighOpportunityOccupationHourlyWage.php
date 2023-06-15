9a89a971c4e94a32b2e95f902e13e3b6ab8d99d6
rjkuyvenhoven <rjkuyvenhoven@gmail.com>
2023-06-15 11:27:00 -0700

    WR 1452 fixes (#364)
    * fix issue with links to career profiles, output "N/A" when Hourly Rate has a value of zero.
    
    * code cleanup

--- src/web/modules/custom/workbc_custom/src/Plugin/views/field/HighOpportunityOccupationHourlyWage.php
+++ src/web/modules/custom/workbc_custom/src/Plugin/views/field/HighOpportunityOccupationHourlyWage.php
@@ -30,7 +30,10 @@ class HighOpportunityOccupationHourlyWage extends FieldPluginBase {
   public function render(ResultRow $values) {
 
     if (!isset($values->high_opportunity_occupations_wage_rate_median) ||
-        $values->high_opportunity_occupations_annual_salary_median == $values->high_opportunity_occupations_wage_rate_median) {
+        $values->high_opportunity_occupations_wage_rate_median == 0 ) {
+      $wage = WORKBC_EXTRA_FIELDS_NOT_AVAILABLE;
+    } 
+    else if ($values->high_opportunity_occupations_annual_salary_median == $values->high_opportunity_occupations_wage_rate_median) {
       $wage = "$" . number_format($values->high_opportunity_occupations_annual_salary_median,0) . "*";
     }
     else {
