1a5f3ce801883b6edbdaac11f95ab57cecd62ab7
rjkuyvenhoven <rjkuyvenhoven@gmail.com>
2023-06-16 11:49:16 -0700

    [WR-1582] french banner editable in cms (#365)
    * add Multilingual link field to Page content type
    
    * create block to output Multilingual link if one is set, update theme to display link
    
    * Multilingual field placement in edit form
    
    * code clean up
    
    * Add missing block categories
    
    * remove old french link include; add new multilingual block to WorkBC Centres
    
    * add WorkBC Centre content type to WorkBC switch language block
    
    ---------
    
    Co-authored-by: infojunkie <karim.ratib@gmail.com>

---
+++ src/web/modules/custom/workbc_custom/src/Plugin/Block/SwitchLanguageBlock.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace Drupal\workbc_custom\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Cache\Cache;
+use Drupal\Core\Url;
+use Drupal\Core\Link;
+/**
+ * Provides a WorkBC Switch language link Block for content that has a value in
+ * the Multilingual centre field.
+ *
+ * @Block(
+ *   id = "switch_language_block",
+ *   admin_label = @Translation("WorkBC switch language block"),
+ *   category = @Translation("WorkBC"),
+ * )
+ */
+class SwitchLanguageBlock extends BlockBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build() {
+    $renderable = array();
+
+    $node = \Drupal::routeMatch()->getParameter('node');
+
+    if (!$node->get('field_multilingual_centre')->isEmpty()) {
+      $target = $node->field_multilingual_centre->getValue()[0];
+      $options = [];
+      $link = Link::fromTextAndUrl($target['title'], Url::fromUri($target['uri'], $options))->toString();
+
+      $renderable = array(
+        '#markup' => $link,
+      );
+    }
+    else {
+      $renderable = array();
+    }
+    return $renderable;
+  }
+
+
+  public function getCacheTags() {
+    // With this when your node change your block will rebuild.
+    if ($node = \Drupal::routeMatch()->getParameter('node')) {
+      // If there is node add its cachetag.
+      return Cache::mergeTags(parent::getCacheTags(), ['node:' . $node->id()]);
+    }
+    else {
+      // Return default tags instead.
+      return parent::getCacheTags();
+    }
+  }
+
+  public function getCacheContexts() {
+    // Every new route this block will rebuild.
+    return Cache::mergeContexts(parent::getCacheContexts(), ['route']);
+  }
+
+}
