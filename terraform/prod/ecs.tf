58b915a094208a85e31282740c08df90d4c149e4
jinchuantang <79226696+jinchuantang@users.noreply.github.com>
2023-06-20 08:58:05 -0700

    Update ecs.tf

--- terraform/prod/ecs.tf
+++ terraform/prod/ecs.tf
@@ -125,6 +125,10 @@ resource "aws_ecs_task_definition" "app" {
 			{
 				name = "REDIS_PORT",
 				value = "6379"
+			},
+			{
+				name = "CF_DIST_ID",
+				value = "${aws_cloudfront_distribution.workbc[0].id}"
 			}
 		]
 		secrets = [
@@ -139,6 +143,10 @@ resource "aws_ecs_task_definition" "app" {
 			{
 				name = "JOBBOARD_GOOGLE_MAPS_KEY",
 				valueFrom = "${data.aws_secretsmanager_secret_version.creds2.arn}:gm_ref::"
+			},
+			{
+				name = "DRUPAL_ADM_PWD",
+				valueFrom = "${data.aws_secretsmanager_secret_version.creds.arn}:drupal_adm_password::"
 			}
 		]
 
