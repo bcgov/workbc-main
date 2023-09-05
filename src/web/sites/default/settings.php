1f9c349a9ca43138e930600786b2ad5eb2ce40ee
infojunkie <karim.ratib@gmail.com>
2023-09-05 16:27:05 -0700

    Refresh Job Board scripts

--- src/web/sites/default/settings.php
+++ src/web/sites/default/settings.php
@@ -788,7 +788,7 @@ $settings['container_yamls'][] = 'modules/contrib/redis/example.services.yml';
 // Job Board settings.
 $config['jobboard']['find_job_url'] = '/search-and-prepare-job/find-jobs';
 $config['jobboard']['find_job_account_url'] = '/account';
-$config['jobboard']['sha'] = '79b4b31cd161df40475a229a4bbeb194829fe681';
+$config['jobboard']['sha'] = '05bae12c67e7c070280f303ab09030c78b02054f';
 
 // File settings.
 $config['file.settings']['make_unused_managed_files_temporary'] = TRUE;
