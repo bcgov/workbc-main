804f7e09a2156876dfafb45571f8a4ab6ac7f70a
rjkuyvenhoven <rjkuyvenhoven@gmail.com>
2023-09-14 15:42:00 -0700

    wip

--- src/web/themes/custom/workbc/js/interactive-map.js
+++ src/web/themes/custom/workbc/js/interactive-map.js
@@ -32,15 +32,19 @@ let currentRegion = {};
         });
 
         $('.map-hot-spot').on('click' , function() {
+          let mapType = $(this).data('interactive-map-type');
           let mapNo = $(this).data('interactive-map-no');
           let regionNo = $(this).data('interactive-map-region-no');
+          let regionLink = $(this).data('interactive-map-region-link');
 
           if (currentRegion[mapNo] != regionNo && currentRegion[mapNo] != 0) {
             var element = context.querySelector(".workbc-interactive-map-" + mapNo + " .interactive-map-" + regions[currentRegion[mapNo]]);
             element.style.visibility = "hidden";
             element.style.display = "none";
             var element2 = context.querySelector(".workbc-interactive-map-" + mapNo + " .interactive-map-row-"+regions[currentRegion[mapNo]]);
-            element2.classList.remove("interactive-map-row-hilite");
+            if (element2 != null) {
+              element2.classList.remove("interactive-map-row-hilite");
+            }
             currentRegion[mapNo] = regionNo;
           }
           // else if (currentRegion[mapNo] == regionNo) {
@@ -53,8 +57,18 @@ let currentRegion = {};
           }
           if (currentRegion[mapNo] != 0) {
             var element = context.querySelector(".workbc-interactive-map-" + mapNo + " .interactive-map-row-"+regions[currentRegion[mapNo]]);
-            element.classList.add("interactive-map-row-hilite");
-            element.scrollIntoView({ block: "center" });
+            if (element != null) {            
+              element.classList.add("interactive-map-row-hilite");
+              element.scrollIntoView({ block: "center" });
+            }
+          }
+
+          if (mapType == "link") {
+            $option = "";
+            if (regionLink.indexOf('http://') != 0 && regionLink.indexOf('https://') != 0) {
+              $option = "_self";
+            }
+            window.open(regionLink, $option);
           }
         });
       });
