9b31025520dca7f5f51fd1a0ff10d436b5c1c1cf
infojunkie <karim.ratib@gmail.com>
2023-09-15 16:29:18 -0700

    Add cache spider

---
+++ src/scripts/test/cache-test.php
@@ -0,0 +1,62 @@
+#!/usr/bin/env php
+<?php
+/**
+ * Iterate over a list of URLs and capture response headers into a CSV.
+ *
+ * Usage:
+ * - wget -q $BASE_URL/sitemap.xml -O sitemap.xml
+ * - xmllint --xpath "//*[local-name()='loc']/text()" sitemap.xml > urls.txt
+ * - ./cache-test.php urls.txt > cache-log.csv
+ */
+$urls = $argv[1];
+$handle = fopen($urls, "r");
+$headers = [
+  "cache-control",
+  "x-drupal-cache",
+  "x-drupal-dynamic-cache",
+  "x-drupal-cache-max-age",
+  "x-served-by",
+  "x-cache",
+  "x-cache-hits",
+  "cf-cache-status",
+  "cf-ray",
+  "cf-request-id",
+];
+if ($handle) {
+  fputcsv(STDOUT, array_merge(["URL"], $headers));
+  while (($url = trim(fgets($handle))) !== false) {
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_HEADER, 1);
+    $response = curl_exec($ch);
+    if (empty($response)) {
+      fwrite(STDERR, curl_error($ch) . "\n");
+      continue;
+    }
+    $header_size = curl_getinfo($ch, CURLINFO_HEADER_SIZE);
+    $response_headers = get_headers_from_curl_response(substr($response, 0, $header_size));
+    fputcsv(STDOUT, array_merge([$url], array_map(function($header) use ($response_headers) {
+      return @$response_headers[$header];
+    }, $headers)));
+    curl_close($ch);
+  }
+  fclose($handle);
+}
+
+function get_headers_from_curl_response($content) {
+  $headers = [];
+  foreach (explode("\r\n", $content) as $i => $line)
+  {
+    if ($i === 0) {
+      $headers['http_code'] = $line;
+    } else {
+      $header = explode(': ', $line);
+      if (count($header) > 1) {
+        list ($key, $value) = $header;
+        $headers[strtolower($key)] = $value;
+      }
+    }
+  }
+  return $headers;
+}
