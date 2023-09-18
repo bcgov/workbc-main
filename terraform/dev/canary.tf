874ff9858558f3817ed59b614ab69c7caf3368fb
guopeiyaopearl <87669294+guopeiyaopearl@users.noreply.github.com>
2023-09-18 12:44:27 -0700

    Update canary.tf

--- terraform/dev/canary.tf
+++ terraform/dev/canary.tf
@@ -1,5 +1,5 @@
 resource "aws_synthetics_canary" "workbc-monitor" {
-  name                 = "workbc-monitor2"
+  name                 = "workbcmonitor2"
   artifact_s3_location = "s3://cw-syn-results-873424993519-ca-central-1/canary/ca-central-1/workbcmonitor2-572-6ce5cc6d373e"
   execution_role_arn   = "arn:aws:iam::873424993519:role/service-role/CloudWatchSyntheticsRole-workbcmonitor2-572-6ce5cc6d373e"
   handler              = "apiCanaryBlueprint.handler"
