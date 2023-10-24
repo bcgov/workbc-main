a1b39603c42f51708f3804cf288b6235e62ccfb4
rjkuyvenhoven <rjkuyvenhoven@gmail.com>
2023-10-24 14:42:12 -0700

    [WBCAMS-151] occupational interest filter drop down and tooltip (#509)
    * wip
    
    * wip
    
    * Tooltip icon placement
    
    * cleanup
    
    * cleanup
    
    * file cleanup

---
+++ src/web/themes/custom/workbc/js/mobile-chosen.js
@@ -0,0 +1,56 @@
+let allSelected = true;
+
+(function ($, Drupal, once) {
+	Drupal.behaviors.mobilechosen = {
+    attach: function (context, settings) {
+
+      once('mobilechosen', '#edit-occupational-interest', context).forEach(function() {
+
+        if (isMobile()) {
+          var select = document.getElementById('edit-occupational-interest');
+          var option = document.createElement('option');
+          option.value = 0;
+          option.innerHTML = "All";
+          option.selected = true;
+          select.insertBefore(option, select.firstChild);
+          
+          oldOptions = select && select.options;
+        }
+
+
+        $('#edit-occupational-interest').on('change' , function() {
+          var select = document.getElementById('edit-occupational-interest');
+          var options = select && select.options;
+
+          if (allSelected) {
+            for (var i=0, iLen=options.length; i<iLen; i++) {
+              opt = options[i];             
+              if (opt.selected && i > 0) {
+                options[0].selected = false;
+                allSelected = false;                
+              }
+            }
+          }
+          else {
+            if (select.selectedIndex == 0) {
+              for (var i=1, iLen=options.length; i<iLen; i++) {
+                opt = options[i];             
+                if (opt.selected && i > 0) {
+                  options[i].selected = false;           
+                }
+              }              
+              allSelected = true;
+            }
+          }
+
+        });
+
+        function isMobile() {
+          const regex = /Mobi|Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i;
+          return regex.test(navigator.userAgent);
+        }
+
+      });
+    }
+  }
+})(jQuery, Drupal, once);
