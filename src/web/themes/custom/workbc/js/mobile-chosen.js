3295a596e97687682f6e2f967bbf4482a0aa7cf4
rjkuyvenhoven <rjkuyvenhoven@gmail.com>
2023-11-23 13:32:28 -0800

    enable  Mobile Detect module, resolve 'illegal choice' issue, resolve '0 selected' issue

--- src/web/themes/custom/workbc/js/mobile-chosen.js
+++ src/web/themes/custom/workbc/js/mobile-chosen.js
@@ -5,44 +5,43 @@ let allSelected = true;
     attach: function (context, settings) {
 
       once('mobilechosen', '#edit-occupational-interest', context).forEach(function() {
-
         if (isMobile()) {
-          var select = document.getElementById('edit-occupational-interest');
-          var option = document.createElement('option');
-          option.value = 0;
-          option.innerHTML = "All";
-          option.selected = true;
-          select.insertBefore(option, select.firstChild);
-          
-          oldOptions = select && select.options;
+          var select = document.getElementById('edit-occupational-interest'); 
+          select.options[0].selected = true;
         }
 
-
         $('#edit-occupational-interest').on('change' , function() {
-          var select = document.getElementById('edit-occupational-interest');
-          var options = select && select.options;
+          
+          if (isMobile()) {         
+            var select = document.getElementById('edit-occupational-interest');
+            var options = select && select.options;
 
-          if (allSelected) {
-            for (var i=0, iLen=options.length; i<iLen; i++) {
-              opt = options[i];             
-              if (opt.selected && i > 0) {
-                options[0].selected = false;
-                allSelected = false;                
+            if (allSelected) {
+              for (var i=0, iLen=options.length; i<iLen; i++) {
+                opt = options[i];             
+                if (opt.selected && i > 0) {
+                  options[0].selected = false;
+                  allSelected = false;                
+                }
+              }
+            }
+            else {
+              if (select.selectedIndex == 0) {
+                for (var i=1, iLen=options.length; i<iLen; i++) {
+                  opt = options[i];             
+                  if (opt.selected && i > 0) {
+                    options[i].selected = false;           
+                  }
+                }              
+                allSelected = true;
+                options[0].selected = true;
+              }
+              else if (select.selectedIndex == -1) {
+                allSelected = true;
+                options[0].selected = true;                
               }
             }
           }
-          else {
-            if (select.selectedIndex == 0) {
-              for (var i=1, iLen=options.length; i<iLen; i++) {
-                opt = options[i];             
-                if (opt.selected && i > 0) {
-                  options[i].selected = false;           
-                }
-              }              
-              allSelected = true;
-            }
-          }
-
         });
 
         function isMobile() {
