0be76f98458c06d02d41f8131f1ee8b4b4d89a8b
rjkuyvenhoven <rjkuyvenhoven@gmail.com>
2023-11-24 10:28:30 -0800

    rework Occupational Interest multi select handling for mobile

--- src/web/themes/custom/workbc/js/mobile-chosen.js
+++ src/web/themes/custom/workbc/js/mobile-chosen.js
@@ -10,39 +10,6 @@ let allSelected = true;
           select.options[0].selected = true;
         }
 
-        $('#edit-occupational-interest').on('change' , function() {
-          if (drupalSettings.isMobile) {         
-            var select = document.getElementById('edit-occupational-interest');
-            var options = select && select.options;
-
-            if (allSelected) {
-              for (var i=0, iLen=options.length; i<iLen; i++) {
-                opt = options[i];             
-                if (opt.selected && i > 0) {
-                  options[0].selected = false;
-                  allSelected = false;                
-                }
-              }
-            }
-            else {
-              if (select.selectedIndex == 0) {
-                for (var i=1, iLen=options.length; i<iLen; i++) {
-                  opt = options[i];             
-                  if (opt.selected && i > 0) {
-                    options[i].selected = false;           
-                  }
-                }              
-                allSelected = true;
-                options[0].selected = true;
-              }
-              else if (select.selectedIndex == -1) {
-                allSelected = true;
-                options[0].selected = true;                
-              }
-            }
-          }
-        });
-
       });
     }
   }
