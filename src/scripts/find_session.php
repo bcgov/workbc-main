5256683484a1be67a543e1c951604172c32908c6
infojunkie <karim.ratib@gmail.com>
2024-02-14 12:00:30 -0800

    [WBCAMS-378] Add find_session script

---
+++ src/scripts/find_session.php
@@ -0,0 +1,33 @@
+<?php
+
+/**
+ * Retrieve session entry for given cookie.
+ * https://drupal.stackexchange.com/a/231726/767
+ *
+ * Usage: drush scr scripts/find_session.php --cookie=<URL-decoded value of SESSxxxx cookie>
+ */
+use Drupal\Component\Utility\Crypt;
+
+$getopt = new \GetOpt\GetOpt([
+  ['c', 'cookie', \GetOpt\GetOpt::REQUIRED_ARGUMENT, 'Cookie to query for session'],
+], []);
+try {
+  $getopt->process($extra);
+}
+catch (Exception $e) {
+  die($getopt->getHelpText() . PHP_EOL);
+}
+$cookie = trim($getopt->getOption('cookie'));
+
+function read($sid) {
+  $data = '';
+  if (!empty($sid)) {
+    // Read the session data from the database.
+    $connection = \Drupal::database();
+    $query = $connection->queryRange('SELECT session FROM {sessions} WHERE sid = :sid', 0, 1, [':sid' => Crypt::hashBase64($sid)]);
+    $data = (string) $query->fetchField();
+  }
+  return $data;
+}
+
+var_dump(read($cookie));
