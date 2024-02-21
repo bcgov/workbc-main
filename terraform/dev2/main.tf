72a1fe0fb3887593682690007dac6dcb2c4c053d
jinchuantang <79226696+jinchuantang@users.noreply.github.com>
2024-02-21 13:03:11 -0800

    Update main.tf

--- terraform/dev2/main.tf
+++ terraform/dev2/main.tf
@@ -2,7 +2,8 @@
 locals {
   common_tags        = var.common_tags
   create_ecs_service = var.app_image == "" ? 0 : 1
-  conn_str           = "http://${data.aws_alb.ssot2.dns_name}:3000"
+  #conn_str           = "http://${data.aws_alb.ssot2.dns_name}:3000"
+  conn_str           = "https://workbc-ssot.b89n0c-dev.nimbus.cloud.gov.bc.ca"
   #TODO
   jb_api_url         = "https://dev-api-jobboard.workbc.ca"
   #TODO
