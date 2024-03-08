cc1497a5735fc7978b1aea5b894eccb63cd9ebba
guopeiyaopearl <87669294+guopeiyaopearl@users.noreply.github.com>
2024-03-08 14:04:14 -0800

    Update dev noc cache policy in cf

--- terraform/dev2/cloudfront.tf
+++ terraform/dev2/cloudfront.tf
@@ -6,11 +6,21 @@ resource "random_integer" "cf_origin_id" {
 }
 
 resource "aws_cloudfront_origin_access_control" "oac-noc" {
-  name = "oac-noc"
+  name = "oac"
   description = "OAC Policy"
   origin_access_control_origin_type = "s3"
   signing_behavior = "always"
   signing_protocol = "sigv4"
+}
+
+data "aws_cloudfront_cache_policy" "custom" {
+    name = "WorkBC-NOC-cache-policy"
+    depends_on = [aws_cloudfront_cache_policy.custom]
+}
+
+data "aws_cloudfront_origin_request_policy" "custom" {
+    name = "WorkBC-NOC-origin-request-policy"
+    depends_on = [aws_cloudfront_origin_request_policy.custom]
 }
 
 
@@ -62,14 +72,18 @@ resource "aws_cloudfront_distribution" "workbc2" {
     cached_methods = ["GET", "HEAD"]
 
     target_origin_id = random_integer.cf_origin_id.result
+    cache_policy_id = aws_cloudfront_cache_policy.custom.id
+    origin_request_policy_id = aws_cloudfront_origin_request_policy.custom.id
 
-    forwarded_values {
-      query_string = true
 
-      cookies {
-        forward = "all"
-      }
-    }
+
+     #forwarded_values {
+     #query_string = true
+
+     #cookies {
+     #   forward = "all"
+     #}
+     #}
 
     viewer_protocol_policy = "redirect-to-https"
     min_ttl                = 0
@@ -143,11 +157,10 @@ resource "aws_cloudfront_distribution" "workbc2" {
 	
     # Associate the CloudFront distribution with the existing WAF web ACL by ARN
     # This regulates users' frequent access to the website
-	#TODO
     #web_acl_id = "arn:aws:wafv2:us-east-1:873424993519:global/webacl/workbc-dev-waf/2bc96095-ff24-4602-95fe-484051c3271e"	 
 }
 
 output "cloudfront_url" {
-  value = "https://${aws_cloudfront_distribution.workbc2[0].domain_name}"
+  value = "https://${aws_cloudfront_distribution.workbc[0].domain_name}"
 }
 
