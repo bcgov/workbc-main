534108c96e5c1adfced352b6fc6d7941712b7cdb
infojunkie <karim.ratib@gmail.com>
2024-05-29 15:55:24 -0700

    [NOC-384] Update Job Board commit hash

--- src/web/sites/default/settings.php
+++ src/web/sites/default/settings.php
@@ -790,7 +790,7 @@ $settings['container_yamls'][] = 'modules/contrib/redis/example.services.yml';
 // Job Board settings.
 $config['jobboard']['find_job_url'] = '/search-and-prepare-job/find-jobs';
 $config['jobboard']['find_job_account_url'] = '/account';
-$config['jobboard']['sha'] = 'db1021f0525ac87906337c93f25d645eac20b85d';
+$config['jobboard']['sha'] = '184eac0c083d7e5271d310b7e7b012b44630ebea';
 
 // File settings.
 $config['file.settings']['make_unused_managed_files_temporary'] = TRUE;
