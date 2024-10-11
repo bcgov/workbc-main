24c433edb645e4fda079be062e2a65f4a3d39dda
infojunkie <karim.ratib@gmail.com>
2024-10-10 18:05:43 -0700

    Fix calendar pagination buttons

--- src/web/modules/custom/calendar_listview/js/cv-custom.js
+++ src/web/modules/custom/calendar_listview/js/cv-custom.js
@@ -21,40 +21,26 @@
         var pager = $(".list-full-view div.pager").clone();
         if($('.list-full-view div.pager2').length <= 0){
           $(once("cv", ".list-full-view div.pager")).after("<div class='pager2'></div>");
-          $(once("cv", ".list-full-view div.pager2")).html(pager);      
+          $(once("cv", ".list-full-view div.pager2")).html(pager);
         }
-        
+
         var dcv = $(".js-drupal-fullcalendar .fc-header-toolbar");
         if(typeof dcv.attr('class') !== "undefined"){
           var pagerCalendarView = dcv.clone();
           if($('.js-drupal-fullcalendar div.pager2').length <= 0){
             $(once("cv", ".fc-view-container")).after("<div class='pager2'></div>");
             $(once("cv", ".js-drupal-fullcalendar div.pager2")).html(pagerCalendarView);
-            $(".js-drupal-fullcalendar div.pager2 .fc-center").hide();         
+            $(".js-drupal-fullcalendar div.pager2 .fc-center").hide();
           }
         }
-        
-        // $('.pager2 .fc-next-button').once().click(function(){
-        //   $('.fc-toolbar .fc-next-button:first').trigger("click");
-        // });
-        // $('.pager2 .fc-prev-button').once().click(function(){
-        //   $('.fc-toolbar .fc-prev-button:first').trigger("click");
-        // });
 
-        if($('.pager2 .fc-next-button').length > 0){
-          console.log("found");
-          console.log($('.pager2 .fc-next-button'));
-          $(once('cv', '.pager2 .fc-next-button', context)).click(function(){
-            console.log("next click");
-            $('.fc-toolbar .fc-next-button:first').trigger("click");
-          });
-        }
+        $(once('cv2', '.pager2 .fc-next-button')).click(function(){
+          $('.fc-toolbar .fc-next-button:first').trigger("click");
+        });
 
-        $('.pager2 .fc-prev-button').click(function(){
-          console.log("prev click");
+        $(once('cv2', '.pager2 .fc-prev-button')).click(function(){
           $('.fc-toolbar .fc-prev-button:first').trigger("click");
-        });   
-
+        });
 
         if (window.innerWidth < 500) {
           $(once("cv", ".fc-day-header.fc-sun")).text("S");
@@ -85,7 +71,7 @@
             content = events[key].des;
           }
         }
-        
+
         $(".fc-day-grid-event").removeClass("active");
         $(this).addClass("active");
         if($(".calendar-full-view .bottom-buttons").height() > 100){
@@ -102,9 +88,9 @@
         }
 
         var element = context.querySelector(".list-view");
-        if (element != null) {            
+        if (element != null) {
           element.scrollIntoView(alignToTop);
-        }        
+        }
       });
     }
   }
