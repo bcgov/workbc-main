39a63407833f6615b3b0af46569600b660b45176
infojunkie <karim.ratib@gmail.com>
2024-12-09 17:41:43 -0800

    [WBCAMS-720] Show visited tabs with Back button

--- src/web/themes/custom/workbc/js/nav-tabs.js
+++ src/web/themes/custom/workbc/js/nav-tabs.js
@@ -5,9 +5,15 @@
     attach: function (context) {
       // Detect URL anchor and activate corresponding tab if found.
       if (window.location.hash) {
-        const triggerEl = $(once('nav_tabs', `a[data-bs-target="${window.location.hash}-content"]`, context));
-        if (triggerEl.length > 0) {
+        const triggerEl = $(`a[data-bs-target="${window.location.hash}-content"]`, context);
+        if (triggerEl.length) {
           bootstrap.Tab.getOrCreateInstance(triggerEl[0]).show();
+        }
+      }
+      else {
+        const triggerEl = $('a[data-bs-target]:first', context);
+        if (triggerEl.length) {
+          window.location.hash = triggerEl.data('bs-target').replace('-content', '');
         }
       }
 
@@ -15,6 +21,16 @@
       $('.nav-link', context).on('shown.bs.tab', function (e) {
         const anchor = $(e.target).attr('href');
         window.location.hash = anchor;
+      });
+
+      // Change tab when navigating to a different anchor.
+      $(window).on('hashchange', function () {
+        const triggerEl = window.location.hash ?
+          $(`a[data-bs-target="${window.location.hash}-content"]`, context) :
+          $('a[data-bs-target]:first', context);
+        if (triggerEl.length && !triggerEl.hasClass('active')) {
+          bootstrap.Tab.getOrCreateInstance(triggerEl[0]).show();
+        }
       });
     },
   };
