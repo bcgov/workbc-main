1e18894da68b70200379570a846a2ec44f801a36
Guo <Pearl.Guo@gov.bc.ca>
2025-02-27 16:08:58 -0800

    fix provider name

--- src/web/sites/default/settings.php
+++ src/web/sites/default/settings.php
@@ -771,6 +771,7 @@ $settings['trusted_host_patterns'] = [
   '^aws-prod\.workbc\.ca$',
   '^dev\.workbc\.ca$',
   '^devnoc\.workbc\.ca$',
+  '^dev3\.workbc\.ca$',
   '^test\.workbc\.ca$',
   //'^www2\.workbc\.ca$',
   '^www\.workbc\.ca$',
@@ -779,6 +780,7 @@ $settings['trusted_host_patterns'] = [
   '^workbc\.b89n0c-prod\.nimbus\.cloud\.gov\.bc\.ca$',
   '^workbc\.b89n0c-dev\.nimbus\.cloud\.gov\.bc\.ca$',
   '^workbc-noc\.b89n0c-dev\.nimbus\.cloud\.gov\.bc\.ca$',
+  '^workbc-dev3\.b89n0c-dev\.nimbus\.cloud\.gov\.bc\.ca$',
   '^workbc\.b89n0c-test\.nimbus\.cloud\.gov\.bc\.ca$',
 ];
 
@@ -801,6 +803,7 @@ $config['workbc']['ssot_repo'] = [
     'local' => 'noc',
     'aws-dev' => 'noc',
     'aws-dev-noc' => 'noc',
+    'aws-dev3' => 'noc',
     'aws-test' => 'ams',
     'aws-prod' => 'master',
   ],
@@ -856,6 +859,7 @@ if (in_array(getenv('PROJECT_ENVIRONMENT'), [
   'dev',
   'aws-dev-noc',
   'aws-dev',
+  'aws-dev3',
   'local'
 ])) {
   $settings['config_exclude_modules'] = [
