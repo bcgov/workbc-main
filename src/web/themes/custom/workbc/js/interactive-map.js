d4dd41dc3845ecc7566efaceaee246dee9113b9c
rjkuyvenhoven <rjkuyvenhoven@gmail.com>
2025-03-24 16:49:51 -0700

    [WBCAMS-974] Interactive map updates
    commit 53d111c67480a1dd916045626dad1709580a14f9
    Merge: 25c1eca2 b2e05a2d
    Author: rjkuyvenhoven <rjkuyvenhoven@gmail.com>
    Date:   Mon Mar 24 16:49:12 2025 -0700
    
        Merge branch 'ams' into WBCAMS-974-interactive-map-svg
    
    commit 25c1eca26f4387c4054c1c26dd948982eab532e2
    Author: rjkuyvenhoven <rjkuyvenhoven@gmail.com>
    Date:   Mon Mar 24 16:48:51 2025 -0700
    
        update map width
    
    commit 0360d19ccbbcd723728c869c12b56bf3fb812f40
    Author: rjkuyvenhoven <rjkuyvenhoven@gmail.com>
    Date:   Mon Mar 24 15:52:16 2025 -0700
    
        recompile sass
    
    commit eac4f78bf40700b3ff5516796dfa1070e443cd87
    Merge: cd797fb1 ec9769d9
    Author: rjkuyvenhoven <rjkuyvenhoven@gmail.com>
    Date:   Mon Mar 24 15:51:01 2025 -0700
    
        Merge branch 'ams' into WBCAMS-974-interactive-map-svg
    
        # Conflicts:
        #	src/web/themes/custom/workbc/css/style.css
        #	src/web/themes/custom/workbc/css/style.css.map
    
    commit cd797fb13822c186fa4ac78c4b1c594876325d4c
    Author: rjkuyvenhoven <rjkuyvenhoven@gmail.com>
    Date:   Mon Mar 24 15:49:28 2025 -0700
    
        convert Interactive Map to use SVG based map

--- src/web/themes/custom/workbc/js/interactive-map.js
+++ src/web/themes/custom/workbc/js/interactive-map.js
@@ -1,57 +1,77 @@
 let regions = ['british_columbia', 'cariboo', 'kootenay', 'mainland_southwest', 'north_coast_nechako', 'northeast','thompson_okanagan', 'vancouver_island_coast'];
 let currentRegion = {};
 
+
 (function ($, Drupal, once) {
 	Drupal.behaviors.interactivemap = {
     attach: function (context, settings) {
 
       once('interactivemap', '.workbc-interactive-map-container', context).forEach(function() {
-        $('.map-hot-spot').each(function() {
+        $('.map-region-label').each(function() {
           let mapNo = $(this).data('interactive-map-no');
           currentRegion[mapNo] = 0;
         });
 
-        $('.map-hot-spot').on('mouseenter' , function() {
+        $('.map-region-label').on('mouseenter' , function() {
           let mapNo = $(this).data('interactive-map-no');
           let regionNo = $(this).data('interactive-map-region-no');
-
-          var element = context.querySelector(".workbc-interactive-map-" + mapNo + " .interactive-map-" + regions[regionNo]);
-          element.style.visibility = "visible";
-          element.style.display = "flex";
+          $(".workbc-interactive-map-"+mapNo).find(".bc-map-region-"+regions[regionNo]).addClass("active");
         });
 
-        $('.map-hot-spot').on('mouseleave' , function() {
+        $('.map-region-label').on('mouseleave' , function() {
           let mapNo = $(this).data('interactive-map-no');
           let regionNo = $(this).data('interactive-map-region-no');
 
           if (regionNo != currentRegion[mapNo]) {
-            var element = context.querySelector(".workbc-interactive-map-" + mapNo + " .interactive-map-" + regions[regionNo]);
-            element.style.visibility = "hidden";
-            element.style.display = "none";
+            $(".workbc-interactive-map-"+mapNo).find(".bc-map-region-"+regions[regionNo]).removeClass("active");
           }
         });
 
-        $('.map-hot-spot').on('click' , function() {
-          let mapType = $(this).data('interactive-map-type');
-          let mapNo = $(this).data('interactive-map-no');
+        $(document).on('click', '.bc-map-region', function() {         
+          let mapType = $(this).closest('.workbc-interactive-map').data('interactive-map-type');
+          let mapNo = $(this).closest('.workbc-interactive-map').data('interactive-map-no');
           let regionNo = $(this).data('interactive-map-region-no');
-          let regionLink = $(this).data('interactive-map-region-link');
 
-          if (currentRegion[mapNo] != regionNo && currentRegion[mapNo] != 0) {
-            var element = context.querySelector(".workbc-interactive-map-" + mapNo + " .interactive-map-" + regions[currentRegion[mapNo]]);
-            element.style.visibility = "hidden";
-            element.style.display = "none";
+          if (currentRegion[mapNo] !== 0) {
+            $(".workbc-interactive-map-"+mapNo).find(".bc-map-region-"+regions[currentRegion[mapNo]]).removeClass("active");
             var element2 = context.querySelector(".workbc-interactive-map-" + mapNo + " .interactive-map-row-"+regions[currentRegion[mapNo]]);
             if (element2 != null) {
               element2.classList.remove("interactive-map-row-hilite");
             }
+          }          
+          $(this).addClass("active");
+          currentRegion[mapNo] = regionNo;
+
+          if (currentRegion[mapNo] !== 0) {
+            var element = context.querySelector(".workbc-interactive-map-" + mapNo + " .interactive-map-row-"+regions[currentRegion[mapNo]]);
+            if (element != null) {            
+              element.classList.add("interactive-map-row-hilite");
+              element.scrollIntoView({ block: "center" });
+            }   
+          }
+
+          if (mapType == "link") {
+            let mapId = $(this).attr('id');
+            let regionLink = $('.map-region-label-' + mapId).data('interactive-map-region-link');
+            window.open(regionLink, "_self");
+          }
+        });        
+
+
+        $('.map-region-label').on('click' , function() {
+          let mapType = $(this).data('interactive-map-type');
+          let mapNo = $(this).data('interactive-map-no');
+          let regionNo = $(this).data('interactive-map-region-no');
+
+          if (currentRegion[mapNo] != regionNo && currentRegion[mapNo] != 0) {
+            $(".workbc-interactive-map-"+mapNo).find(".bc-map-region-"+regions[currentRegion[mapNo]]).removeClass("active");
+            var element2 = context.querySelector(".workbc-interactive-map-" + mapNo + " .interactive-map-row-"+regions[currentRegion[mapNo]]);
+            if (element2 != null) {
+              element2.classList.remove("interactive-map-row-hilite");
+            }
+            $(this).addClass("active");
             currentRegion[mapNo] = regionNo;
           }
-          // else if (currentRegion[mapNo] == regionNo) {
-          //   var element2 = context.querySelector(".workbc-interactive-map-" + mapNo + " .interactive-map-row-"+regions[currentRegion[mapNo]]);
-          //   element2.classList.remove("interactive-map-row-hilite");
-          //   currentRegion[mapNo] =  0;
-          // }
           else {
             currentRegion[mapNo] = regionNo;
           }
@@ -64,6 +84,7 @@ let currentRegion = {};
           }
 
           if (mapType == "link") {
+            let regionLink = $(this).data('interactive-map-region-link');
             window.open(regionLink, "_self");
           }
         });
