6d80e96c2bd691281c03e5b121fb3bc7e782082d
infojunkie <karim.ratib@gmail.com>
2025-04-09 12:31:24 -0700

    Automatically close other categories when one is opened

--- src/web/modules/custom/workbc_custom/js/explore-grid.js
+++ src/web/modules/custom/workbc_custom/js/explore-grid.js
@@ -1,14 +1,19 @@
 (function ($, Drupal, once) {
   Drupal.behaviors.feedback = {
     attach: function (context, settings) {
-      $(once('explore', '#workbc-custom-explore-careers-grid-form .grid-all')).change(function() {
+      $(once('explore-grid', '#workbc-custom-explore-careers-grid-form .grid-all')).change(function() {
         const checked = $(this).is(':checked');
         const parent = $(this).closest('.details-wrapper');
         $(parent).find('.grid-term').prop('checked', checked);
       });
-      $(once('explore', '#workbc-custom-explore-careers-grid-form .grid-term')).change(function() {
+      $(once('explore-grid', '#workbc-custom-explore-careers-grid-form .grid-term')).change(function() {
         const parent = $(this).closest('.details-wrapper');
         $(parent).find('.grid-all').prop('checked', false);
+      });
+      $(once('explore-grid', 'details')).on('toggle', function() {
+        if (this.open) {
+          $(this).siblings('details').removeAttr('open');
+        }
       });
     }
   }
