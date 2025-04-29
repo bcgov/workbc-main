d691143ed3f6bb38784f64d23599967d2db4174b
anuragparihar1 <anurag@parihar.ca>
2025-04-29 09:25:45 -0700

    career-trek-mobile-changes-v2

--- src/web/themes/custom/workbc/js/career-inner-video.js
+++ src/web/themes/custom/workbc/js/career-inner-video.js
@@ -1,39 +1,50 @@
 (function ($, Drupal, once) {
     "use strict";
-
+  
     Drupal.behaviors.careerInnerVideo = {
       attach: function (context, settings) {
         // Autoplay on video click
         once('careerInnerVideo', '.profile-video', context).forEach(function (element) {
           element.onclick = function () {
             $(this).addClass('active');
+            
+              
             const iframe = $(this).find('iframe');
-            console.log(iframe);
+            $('.profile-video')[0].offsetHeight;
+
+            setTimeout(() => {
+                iframe.css({
+                    'position': 'absolute',
+                    'display': 'block',
+                    'z-index': '1'
+                })
+            }, 200);
+            // Force reflow
 
             if (iframe.length) {
-              let src = iframe.attr('src') || '';
-              if (!src.includes('autoplay=1')) {
-                const connector = src.includes('?') ? '&' : '?';
-                const newSrc = `${src}${connector}autoplay=1&mute=1`;
-
-                iframe.attr('src', newSrc);
-                iframe.attr('allow', 'autoplay; encrypted-media'); // <-- THIS IS KEY
-                console.log("YouTube autoplay triggered");
-              }
+            //   let src = iframe.attr('src') || '';
+            //   if (!src.includes('autoplay=1')) {
+            //     const connector = src.includes('?') ? '&' : '?';
+            //     const newSrc = `${src}${connector}autoplay=1&mute=1`;
+  
+            //     iframe.attr('src', newSrc);
+            //     iframe.attr('allow', 'autoplay; encrypted-media'); // <-- THIS IS KEY
+            //     console.log("YouTube autoplay triggered");
+            //   }
             } else {
               console.warn('No iframe found inside .profile-video');
             }
           };
         });
-
+  
         // Sticky scroll behavior
         const $topVideoRow = $('.top-video-row', context);
         if ($topVideoRow.length) {
           const originalOffsetTop = $topVideoRow.offset().top;
-
+  
           $(window).on('scroll.careerInnerVideoSticky', function () {
             const scrollTop = $(window).scrollTop();
-
+  
             if (scrollTop >= originalOffsetTop) {
               $topVideoRow.css({
                 'position': 'sticky',
@@ -42,7 +53,6 @@
                 'z-index': '499',
                 'background': '#fff'
               });
-              console.log("test")
             } else {
               $topVideoRow.css({
                 'position': 'static',
@@ -54,3 +64,4 @@
       }
     };
   })(jQuery, Drupal, once);
+  
