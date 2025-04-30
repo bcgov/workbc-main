35238e0079ac211c5953f9ee13aa584430c9f140
infojunkie <karim.ratib@gmail.com>
2025-04-30 10:23:46 -0700

    [WBCAMS-772] Adjust search bar layout, better

--- src/web/modules/custom/workbc_custom/src/Form/ExploreCareersSearchForm.php
+++ src/web/modules/custom/workbc_custom/src/Form/ExploreCareersSearchForm.php
@@ -41,12 +41,14 @@ class ExploreCareersSearchForm extends FormBase {
         'arguments' => [],
         'filter' => 'search',
       ],
+      '#prefix' => '<div class="single-row">',
       '#default_value' => $form_state->getValue('keywords'),
       '#attributes' => ['placeholder' => $this->t('Enter a keyword or NOC code')],
     ];
     $form['submit'] = [
       '#type' => 'submit',
       '#value' => $this->t('Search'),
+      '#suffix' => '</div>',
     ];
     return $form;
   }
