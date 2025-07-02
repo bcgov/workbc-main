af00415bdfbcee77faa0bfbaa5074ac1608ecae1
vmuchalla <148374832+vmuchalla@users.noreply.github.com>
2025-07-02 13:25:32 -0700

    Update career-count-video.js (#772)
    * Update career-count-video.js
    
    * Update workbc.libraries.yml
    
    * Update workbc.info.yml

--- src/web/themes/custom/workbc/js/career-count-video.js
+++ src/web/themes/custom/workbc/js/career-count-video.js
@@ -1,1 +1,15 @@
-
+(function ($, Drupal) {
+	Drupal.behaviors.updateResultSummary = {
+		attach: function (context, settings) {
+			function updateSummary() {
+				const $view = $('.view-id-career-trek-video-library.view-display-id-default');
+				const count = $view.find('.views-row').length;
+				$('.result-summary .update-result').text(count);
+			}
+			
+			$(document).ready(updateSummary);
+			
+			$(document).ajaxComplete(updateSummary);
+		}
+	};
+})(jQuery, Drupal);
