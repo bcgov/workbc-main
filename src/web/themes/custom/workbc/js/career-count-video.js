5bb9a150100303cd5b651982769840bce09cb31c
infojunkie <karim.ratib@gmail.com>
2025-07-11 12:25:16 -0700

    Update function name and remove console logs

--- src/web/themes/custom/workbc/js/career-count-video.js
+++ src/web/themes/custom/workbc/js/career-count-video.js
@@ -1,42 +1,36 @@
 (function ($, Drupal) {
-	Drupal.behaviors.updateResultSummary = {
-		attach: function (context, settings) {
-			function updateSummary() {
-				const $view = $('.view-id-career_trek_video_library.view-display-id-block_1');
-				if (!$view.length) {
-					console.warn('View not found.');
-					return;
-				}
-				
-				setTimeout(function () {
-				const $rows = $view.find('.view-row');
-				const count = $rows.length;
-				
-				console.log('Loaded rows after delay:', count);
-				
-				const $summary = $('.result-summary .update-result');
-				if (!$summary.length) {
-					console.warn('Result summary span not found.');
-					return;
-				}
-				
-				$summary.text(count);
-				
-				const $summaryWrapper = $view.find('.result-summary');
-				const $loadMoreLink = $view.find('a.btn-primary[title="Load more items"]').last();
-				
-				if ($summaryWrapper.length && $loadMoreLink.length) {
-					$loadMoreLink.after($summaryWrapper);
-				}
-			}, 100);
-		}
-		
-		
-		$(document).ready(updateSummaryAndMove);
-		
-		$(document).ajaxComplete(function () {
-			updateSummaryAndMove();
-		});
-	}
-	};
+  Drupal.behaviors.updateResultSummary = {
+    attach: function (context, settings) {
+      function updateSummaryAndMove() {
+        const $view = $('.view-id-career_trek_video_library.view-display-id-block_1');
+        if (!$view.length) {
+          return;
+        }
+
+        setTimeout(function () {
+          const $rows = $view.find('.view-row');
+          const count = $rows.length;
+          const $summary = $('.result-summary .update-result');
+          if (!$summary.length) {
+            return;
+          }
+
+          $summary.text(count);
+
+          const $summaryWrapper = $view.find('.result-summary');
+          const $loadMoreLink = $view.find('a.btn-primary[title="Load more items"]').last();
+
+          if ($summaryWrapper.length && $loadMoreLink.length) {
+            $loadMoreLink.after($summaryWrapper);
+          }
+        }, 100);
+      }
+
+      $(document).ready(updateSummaryAndMove);
+
+      $(document).ajaxComplete(function () {
+        updateSummaryAndMove();
+      });
+    }
+  };
 })(jQuery, Drupal);
