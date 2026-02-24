0a96ef8ecb5c035db4fd5678ff02d9a366c11411
infojunkie <karim.ratib@gmail.com>
2026-02-23 18:44:16 -0800

    Language toggle block

---
+++ src/web/modules/custom/workbc_custom/src/Plugin/Block/LanguageToggleBlock.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Drupal\workbc_custom\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+
+/**
+ * Provides a WorkBC language toggle block.
+ *
+ * @Block(
+ *   id = "language_toggle_block",
+ *   admin_label = @Translation("WorkBC language toggle block"),
+ *   category = @Translation("WorkBC"),
+ * )
+ */
+class LanguageToggleBlock extends BlockBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build() {
+    // It's empty because the template will take care of everything.
+    return ['#markup' => ''];
+  }
+}
