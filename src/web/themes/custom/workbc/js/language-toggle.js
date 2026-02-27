706647959b1cb4f237b8440f9803216b01620bd2
infojunkie <karim.ratib@gmail.com>
2026-02-26 17:35:27 -0800

    [WBCAMS-1667] Preserve language choice across pages

--- src/web/themes/custom/workbc/js/language-toggle.js
+++ src/web/themes/custom/workbc/js/language-toggle.js
@@ -6,6 +6,10 @@
 (function (Drupal, $, once) {
 	Drupal.behaviors.languageToggle = {
     attach: function (context, settings) {
+      once('languageToggle', 'html', context).forEach(function() {
+        window.addEventListener('load', pageLoad);
+      });
+
       once('languageToggle', 'body.search-and-prepare-jobfind-jobs', context).forEach(function() {
         window.addEventListener('load', jobFindPageChanges);
         window.addEventListener('hashchange', jobFindPageChanges);
@@ -16,17 +20,19 @@
       // document.querySelectorAll(u_class).forEach(function(e){e.addEventListener('pointerenter',load_tlib)});
       function load_tlib(){if(!window.gt_translate_script){window.gt_translate_script=document.createElement('script');gt_translate_script.src='https://translate.google.com/translate_a/element.js?cb=googleTranslateElementInit2';document.body.appendChild(gt_translate_script);}}
 
-      const cookie_lang = document.cookie
+      const googtrans = document.cookie
         .split("; ")
         .find((row) => row.startsWith("googtrans="))
         ?.split("=")[1];
-      let isFrench = 'languageToggle' in settings ? settings.languageToggle.isFrench : cookie_lang === '/en/fr';
-      $('.language-toggle input', context).prop('checked', isFrench);
+      let isFrench = 'languageToggle' in settings ? settings.languageToggle.isFrench : googtrans === '/en/fr';
+
       $(once('languageToggle', '.language-toggle', context)).on('click', () => {
         const checked = $('.language-toggle input', context).is(':checked');
         if (checked !== isFrench) {
           if ('languageToggle' in settings) {
+            window.sessionStorage.setItem('WorkBC.languageToggle', checked ? 'fr' : 'en');
             window.location = settings.languageToggle.url;
+            return;
           }
           else {
             load_tlib();
@@ -45,12 +51,48 @@
       });
 
       $(once('languageToggle', '.gt_selector', context)).on('change', (e) => {
-        if (!('languageToggle' in settings)) {
-          const lang = $(e.target).val();
-          isFrench = lang === 'en|fr';
-          $('.language-toggle input', context).prop('checked', isFrench);
+        const lang = $(e.target).val();
+        if ('languageToggle' in settings) {
+          if (
+            (lang === 'en|fr' && !settings.languageToggle.isFrench) ||
+            (lang === 'en|en' && settings.languageToggle.isFrench)
+          ) {
+            window.sessionStorage.setItem('WorkBC.languageToggle', lang.split('|')[1]);
+            window.location = settings.languageToggle.url;
+            return;
+          }
         }
+        isFrench = lang === 'en|fr';
+        $('.language-toggle input', context).prop('checked', isFrench);
       });
+
+      function pageLoad() {
+        $('.language-toggle input', context).prop('checked', isFrench);
+
+        if ('languageToggle' in settings) {
+          // Redirect to correct language if needed.
+          const wasFrench = window.sessionStorage.getItem('WorkBC.languageToggle') === 'fr';
+          if (wasFrench != isFrench) {
+            window.location = settings.languageToggle.url;
+            return;
+          }
+
+          // Reset the Google Translate widget.
+          load_tlib();
+          $('.gt_selector', context).val('en|en');
+          window.doGTranslate('en|en');
+        }
+        else {
+          // Set the Google Translate widget.
+          load_tlib();
+          const lang = isFrench ? 'en|fr' : 'en|en';
+          $('.gt_selector', context).val(lang);
+          window.doGTranslate(lang);
+        }
+
+        // Remember current language setting.
+        window.sessionStorage.setItem('WorkBC.languageToggle', isFrench ? 'fr' : 'en');
+      }
 
       function jobFindPageChanges() {
         if (window.location.hash.startsWith('#/job-details/')) {
