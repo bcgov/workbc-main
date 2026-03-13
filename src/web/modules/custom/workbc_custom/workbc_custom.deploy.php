ea3f8e012b239e75415b7b5005d2d9e60416d491
rjkuyvenhoven <rjkuyvenhoven@gmail.com>
2026-03-13 16:24:26 -0700

    import WorkBC Centre regions

--- src/web/modules/custom/workbc_custom/workbc_custom.deploy.php
+++ src/web/modules/custom/workbc_custom/workbc_custom.deploy.php
@@ -60,3 +60,50 @@ function workbc_custom_deploy_1204_mismatch_fix(&$sandbox = NULL) {
   \Drupal::service("meaofd.fixer")->fix("media");
   return t("[WBCAMS-1204] fix The Drupal Media entity type issues.");
 }
+
+
+/**
+ * Fix mismatch entity type.
+ *
+ * As per ticket WBCAMS-1717
+ */
+function workbc_custom_deploy_1717_import_centre_regions(&$sandbox = NULL) {
+
+  if (!isset($sandbox['centres'])) {
+    // load remote videos
+    $database = \Drupal::database();
+
+    $module_path = \Drupal::service('extension.path.resolver')->getPath('module', 'workbc_custom');
+    $file_path = $module_path . '/data/centres_regions.csv';
+    if (file_exists($file_path)) {
+      if (($handle = fopen($file_path, 'r')) !== FALSE) {
+        $data = [];
+        while (($row = fgetcsv($handle, 1000, ',')) !== FALSE) {
+          $data[] = $row;
+        }
+        fclose($handle);
+      }
+    }
+    $sandbox['centres'] = $data;
+    $sandbox['count'] = count($sandbox['centres']);
+  }
+
+  $message = "No action taken.";
+  $centre = array_shift($sandbox['centres']);
+
+  $entity_type_manager = \Drupal::entityTypeManager();
+
+  // Load all nodes matching the title
+  $nodes = $entity_type_manager->getStorage('node')->loadByProperties(['title' => $centre[0]]);
+  if (!empty($nodes)) {
+    foreach ($nodes as $node) {
+      // You can then access its properties, e.g.,
+      $node->set('field_region', $centre[1]);
+      $node->save();
+    }
+    $message = "WorkBC Centre: " . $centre[0] . " - region set to " . $centre[1];
+  }
+
+  $sandbox['#finished'] = empty($sandbox['centres']) ? 1 : ($sandbox['count'] - count($sandbox['centres'])) / $sandbox['count'];
+  return t("[WBCAMS-1717] $message");
+}
