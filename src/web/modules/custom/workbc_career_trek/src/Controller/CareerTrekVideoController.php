f0fff4beee941e380a12f3bc4b561a811cd1247d
rjkuyvenhoven <rjkuyvenhoven@gmail.com>
2026-04-02 13:39:26 -0700

    [WBCAMS-1809] Career trek redux video page (#855)
    * career trek video page
    
    * career trek video detail page update, disable Career Trek Node view, update ssotCareerProfile(), update workbc_extra_fields_node_view()
    
    * remove workbc_career_trek_node_view function

---
+++ src/web/modules/custom/workbc_career_trek/src/Controller/CareerTrekVideoController.php
@@ -0,0 +1,57 @@
+<?php
+
+/**
+ * @file
+ * CustomController class.
+ */
+
+namespace Drupal\workbc_career_trek\Controller;
+
+use Drupal\Core\Controller\ControllerBase;
+use Drupal\Core\Database\Query\ConditionInterface;
+use Drupal\node\Entity\Node;
+use Drupal\media\Entity\Media;
+
+class CareerTrekVideoController extends ControllerBase {
+
+  public function content($noc, $episode) {
+    $entityStorage = \Drupal::entityTypeManager()->getStorage('node');
+    $query = $entityStorage->getQuery();
+    $query->condition('type', 'career_profile');
+    $query->condition('field_noc', $noc, '=');
+    $query->condition('status', 1);
+    $result = $query->accessCheck(false)->execute();
+    $node = Node::load($result[array_key_first($result)]);
+    $node->episode_number = $episode;
+
+    $view_builder = \Drupal::entityTypeManager()->getViewBuilder('node');
+    $render_array = $view_builder->view($node, 'full_career_trek_videos');
+    \Drupal::service('renderer')->renderRoot($render_array);
+
+    $content = [
+      '#theme' => 'career_trek_video_detail',
+      '#noc' => $noc,
+      '#episode' => $episode,
+      '#node' => $node,
+      '#content' => $render_array,
+      '#view_mode' => 'full-career-trek-videos',
+    ];
+
+    return $content;
+  }
+
+  public function getTitle($noc, $episode) {
+    $title = "";
+    $mediaStorage = \Drupal::entityTypeManager()->getStorage('media');
+    $query = $mediaStorage->getQuery();
+    $query->condition('bundle', 'remote_video');
+    $query->condition('field_episode', $episode, '=');
+    $query->condition('status', 1);
+    $result = $query->accessCheck(false)->execute();
+    $video = Media::load($result[array_key_first($result)]);
+    if ($video) {
+      $title = $video->getName();
+    }
+    return $title;
+  }
+}
