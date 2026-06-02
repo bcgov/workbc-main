ab1b42ad7d1eb7ec4d6c32ec91942e27d6f00443
infojunkie <karim.ratib@gmail.com>
2026-06-02 13:02:38 -0700

    [WBCAMS-1913] Add missing quick links

--- src/web/themes/custom/workbc/js/mobile-nav.js
+++ src/web/themes/custom/workbc/js/mobile-nav.js
@@ -4,13 +4,31 @@
   // Mobile Nav Close
   Drupal.behaviors.mobileNavClose = {
     attach: function (context, settings) {
-      const offCanvas = $("#off-canvas")[0];
+      const offCanvas = document.querySelector("#off-canvas");
       const mmenuApi = offCanvas.mmApi;
 
       $(once("mobileNavClose", ".mobile-nav-close", context)).on('click', function() {
         var opened = offCanvas.classList.contains("mm-menu--opened");
         // Trigger the open or close method of mmenu.js.
         mmenuApi[opened ? "close" : "open"]();
+      });
+
+      function updateSplash(panel) {
+        const $parent = $(`#${panel.dataset.mmParent}`, offCanvas);
+        if ($parent.length > 0 && $parent[0].dataset.splash) {
+          if (!$(".megamenu-splash", panel).length) {
+            $(panel).append(`<div class="megamenu-splash">${$parent[0].dataset.splash}</div>`);
+          }
+        }
+      }
+
+      $(once("mobileNavClose", "#off-canvas", context)).each(() => {
+        mmenuApi.bind('open:after', () => {
+          updateSplash($(".mm-panel--opened", offCanvas)[0]);
+        });
+        mmenuApi.bind('openPanel:before', (panel) => {
+          updateSplash(panel);
+        });
       });
 
       $(once("mobileNavClose", ".menu-name--account > a", context))
