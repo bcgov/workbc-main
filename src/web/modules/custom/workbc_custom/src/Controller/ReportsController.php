995e20211f0ce549f75b6464dd75bdb202180532
rjkuyvenhoven <rjkuyvenhoven@gmail.com>
2026-06-17 12:59:44 -0700

    wbcams-1997 report (#876)

--- src/web/modules/custom/workbc_custom/src/Controller/ReportsController.php
+++ src/web/modules/custom/workbc_custom/src/Controller/ReportsController.php
@@ -8,6 +8,7 @@
 namespace Drupal\workbc_custom\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
+use Drupal\media\Entity\Media;
 
 class ReportsController extends ControllerBase {
   public function environment() {
@@ -19,4 +20,47 @@ class ReportsController extends ControllerBase {
       '#markup' => $env,
     ];
   }
+
+
+  public function wbcams_1997() {
+    $content = "";
+
+    $database = \Drupal::database();
+
+    $query = $database->select('media_field_data', 'm');
+    $query->fields('m', ['mid', 'name', 'status', 'created', 'bundle']);
+    $or_group = $query->orConditionGroup()
+      ->condition('m.bundle', 'image', '=')
+      ->condition('m.bundle', 'icon', '=');
+    $query->condition($or_group);
+    $images = $query->execute()->fetchAll();
+    $total = count($images);
+
+    $list = [];
+    foreach ($images as $image) {
+      $media = Media::load($image->mid);
+      if ($image->bundle == "icon") {
+        $image_value = $media->get('field_media_image_1')->getValue();
+      }
+      else {
+        $image_value = $media->get('field_media_image')->getValue();
+      }
+      if (!empty($image_value) && strtolower($image_value[0]['alt']) == "alt") {
+        $list[] = $image;
+      }
+    }
+
+    $content .= "<p>Total Images: " . $total . "</p>";
+    $content .= "<p>images/icons with Alternative text 'alt': " . count($list) . "</p>";
+    $content .= "<br>";
+    foreach ($list as $alt) {
+      $content .= '<p><a href="/media/' . $alt->mid . '/edit">[' . $alt->mid . '] - ' . $alt->name . '</a></p>';
+    }
+
+    return [
+      '#type' => 'markup',
+      '#markup' => $content,
+    ];
+  }
+
 }
