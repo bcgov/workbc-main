5d869add48b6ee1d0f87a69d69ea89e2b5c555fa
infojunkie <karim.ratib@gmail.com>
2026-07-28 16:18:18 -0700

    [WBCAMS-1266] Back to top custom behaviour for Career Trek

--- src/web/themes/custom/workbc/js/back-to-top.js
+++ src/web/themes/custom/workbc/js/back-to-top.js
@@ -1,54 +1,65 @@
 (function (Drupal, $, once) {
-    ("use strict");
+  ("use strict");
 
-    const scrollToTop = () => {
-        window.scrollTo({top: 0, behavior: 'smooth'});
+  let backToTopId = null;
+
+  const scrollToTop = () => {
+    let backToElement = null;
+    if (backToTopId && (backToElement = document.getElementById(backToTopId))) {
+      backToElement.scrollIntoView(true);
+    }
+    else {
+      window.scrollTo({ top: 0, behavior: 'smooth' });
+    }
+  };
+
+  const initScrollToTopTrigger = () => {
+    var trigger = injectFixedButton();
+    attachScrollListener(trigger);
+  };
+
+  const injectFixedButton = () => {
+    var container = document.createElement('div');
+    container.classList.add('back-to-top');
+    container.setAttribute('role', 'navigation');
+    container.setAttribute('aria-label', 'Back to top');
+    document.body.appendChild(container);
+
+    var label = document.createElement('label');
+    label.textContent = 'Back to top';
+    label.classList.add('visually-hidden');
+    container.appendChild(label);
+
+    var trigger = document.createElement('button');
+    trigger.textContent = 'Back to top';
+    trigger.classList.add('back-to-top__trigger', 'btn', 'btn-prefix-chevron-up');
+    trigger.addEventListener('click', scrollToTop);
+    container.appendChild(trigger);
+
+    return trigger;
+  };
+
+  const attachScrollListener = (triggerElem) => {
+
+    var onScrollBehavior = function() {
+      var y = window.scrollY;
+      if (y >= 150) {
+        triggerElem.classList.add('active');
+      } else {
+        triggerElem.classList.remove('active');
+      }
     };
 
-    const initScrollToTopTrigger = () => {
-        var trigger = injectFixedButton();
-        attachScrollListener(trigger);
-    };
+    window.addEventListener("scroll", onScrollBehavior);
+  };
 
-    const injectFixedButton = () => {
-        var container = document.createElement('div');
-        container.classList.add('back-to-top');
-        container.setAttribute('role', 'navigation');
-        container.setAttribute('aria-label', 'Back to top');
-        document.body.appendChild(container);
+  Drupal.behaviors.back_to_top = {
+    attach: function (context, settings) {
+      if ('back_to_top' in settings) {
+        backToTopId = settings.back_to_top;
+      }
+      $(once('back_to_top', 'html', context)).each(initScrollToTopTrigger);
+    },
+  };
 
-        var label = document.createElement('label');
-        label.textContent = 'Back to top';
-        label.classList.add('visually-hidden');
-        container.appendChild(label);
-
-        var trigger = document.createElement('button');
-        trigger.textContent = 'Back to top';
-        trigger.classList.add('back-to-top__trigger', 'btn', 'btn-prefix-chevron-up');
-        trigger.addEventListener('click', scrollToTop);
-        container.appendChild(trigger);
-
-        return trigger;
-    };
-
-    const attachScrollListener = (triggerElem) => {
-
-        var onScrollBehavior = function() {
-          var y = window.scrollY;
-          if (y >= 150) {
-            triggerElem.classList.add('active');
-          } else {
-            triggerElem.classList.remove('active');
-          }
-        };
-
-        window.addEventListener("scroll", onScrollBehavior);
-    };
-
-    Drupal.behaviors.back_to_top = {
-      attach: function (context, settings) {
-        $(once('back_to_top', 'html', context)).each(initScrollToTopTrigger);
-      },
-    };
-
-  })(Drupal, jQuery, once);
+})(Drupal, jQuery, once);
