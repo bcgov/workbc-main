188cf03fa3a43067e88f2e90b047350de0505b2c
infojunkie <karim.ratib@gmail.com>
2026-09-02 13:13:56 -0700

    [WBCAMS-2138] Add tooltip label

--- src/web/themes/custom/workbc/js/init-popovers.js
+++ src/web/themes/custom/workbc/js/init-popovers.js
@@ -3,28 +3,41 @@
 
   // this set of functions is intended to cause popovers to close if the user clicks anywhere outside of them
   // see https://stackoverflow.com/a/69602400/495000
-  let managePopoverClosure = function () {
+  const managePopoverClosure = function () {
     $(document).on('click', function (e) {
-      var $popover,
-      $target = $(e.target);
+      const $target = $(e.target);
       //do nothing if there was a click on popover content
       if ($target.hasClass('popover') || $target.closest('.popover').length) {
         return;
       }
       $('[data-bs-toggle="popover"]').each(function () {
-        $popover = $(this);
-
-        if (!$popover.is(e.target) &&
-        $popover.has(e.target).length === 0 &&
-        $('.popover').has(e.target).length === 0)
-        {
+        const $popover = $(this);
+        if (
+          !$popover.is(e.target) &&
+          $popover.has(e.target).length === 0 &&
+          $('.popover').has(e.target).length === 0
+        ) {
           $popover.popover('hide');
         }
       });
-    })
+    });
+
+    $(document).on('keyup', function(event) {
+      if (event.key == "Escape") {
+        $('[data-bs-toggle="popover"]').each(function () {
+          $(this).popover('hide');
+        });
+      }
+    });
+
+    $(document).on('blur', '[data-bs-toggle="popover"]', function(event) {
+      if (!event.relatedTarget || $(event.relatedTarget).parents('.popover').length == 0) {
+        $(this).popover('hide');
+      }
+    });
   }
 
-  let initPopovers = function () {
+  const initPopovers = function () {
     $(document).ready(function() {
       managePopoverClosure();
       $('[data-bs-toggle="popover"]').popover();
