c4b37d733dcebb1ed53735e5359fbd2174b74b31
infojunkie <karim.ratib@gmail.com>
2026-09-16 15:50:33 -0700

    [WBCAMS-2157] Fix tooltip link

--- src/web/themes/custom/workbc/js/init-popovers.js
+++ src/web/themes/custom/workbc/js/init-popovers.js
@@ -44,14 +44,17 @@
     attach: function (context, settings) {
       $(once('initPopoverBehaviour', '.info-tooltip', context)).each(function() {
         const $element = $(this);
+        const role = $element.data('bs-content').includes('<a href') ? 'dialog' : 'tooltip';
         $(document).ready(function() {
           managePopoverClosure(context);
           $element.on('shown.bs.popover', function (event) {
-            const $element = $(event.target);
-            $('#tooltip-live-region').html($element.attr('data-bs-original-title') + $element.attr('data-bs-content'));
+            const $target = $(event.target);
+            $('#tooltip-live-region').html($target.attr('data-bs-original-title') + $target.attr('data-bs-content'));
           }).on('hidden.bs.popover', function (event) {
             $('#tooltip-live-region').text('');
-          }).popover();
+          }).popover({
+            template: `<div class="popover" role="${role}"><div class="popover-arrow"></div><h3 class="popover-header"></h3><div class="popover-body"></div></div>`
+          });
         });
       });
     },
